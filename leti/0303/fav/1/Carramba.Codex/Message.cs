842b1bed15f5a0bc2f1eed33db8cd2e1ae73f1a5
sewa312 <sewa080@mail.ru>
2016-02-23 00:45:51 +0300

    first lab server implementation (YARRRR)

---
+++ leti/0303/fav/1/Carramba.Codex/Message.cs
@@ -0,0 +1,20 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carramba.Codex
+{
+    [ProtoContract]
+    public class Message
+    {
+        [ProtoMember(1)]
+        public string Sender { get; set; }
+        [ProtoMember(2)]
+        public string Text { get; set; }
+        [ProtoMember(3, IsRequired = false)]
+        public string Data { get; set; }
+    }
+}
---
+++ leti/0303/fav/1/Carramba.Tortuga/Server.cs
@@ -0,0 +1,64 @@
+﻿using Carramba.Codex;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carramba.Tortuga
+{
+    class Server
+    {
+        ConcurrentDictionary<TcpClient, Connection> clientBase = new ConcurrentDictionary<TcpClient, Connection>();
+
+        public Server()
+        {
+
+        }
+
+        public Task Start()
+        {
+            return Listen();
+        }
+
+        private async Task Listen()
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 10000);
+            tcpListener.Start();
+
+            while (true)
+            {
+                TcpClient client = await tcpListener.AcceptTcpClientAsync();
+                var unnecessary = WorkWith(client).ContinueWith(task=>ClientDisconnection(task, client));
+            }  
+        }
+
+        private async Task WorkWith(TcpClient client)
+        {
+            var connection = new Connection(client); 
+            clientBase.TryAdd(client, connection);
+            while (true)
+            {
+                Message message = await connection.ReadMessage();
+                foreach (Connection con in clientBase.Values)
+                {
+                    try
+                    {
+                        await con.SendMessage(message);
+                    }
+                    catch (Exception ex) { Console.WriteLine("POLUNDRA!!!: {0}", ex); }
+                }
+
+            }
+        }
+
+        private void ClientDisconnection(Task zabort, TcpClient client)
+        {
+            Connection connect;
+            clientBase.TryRemove(client, out connect);
+        }
+    }
+}
