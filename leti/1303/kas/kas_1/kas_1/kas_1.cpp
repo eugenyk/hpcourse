20485d2ffca72a8331c248f717dd78ca506332b9
oxaoo <yol0317@gmail.com>
2016-12-04 06:39:37 +0300

    added counting the input brightness value

--- leti/1303/kas/kas_1/kas_1/kas_1.cpp
+++ leti/1303/kas/kas_1/kas_1/kas_1.cpp
@@ -36,15 +36,36 @@ struct min_brightness {
 	}
 };
 
-class res {
-	int &minb, &maxb;
+class cnt_brightness {
+	unsigned char input_brightness;
 
 public:
-	res(int &minb, int &maxb) : minb(minb), maxb(maxb) {}
-	int operator()(tuple<int, int> v) {
-		printf("res %d and %d\n", get<0>(v), get<1>(v));
+	cnt_brightness(unsigned char input_brightness) : input_brightness(input_brightness) {}
+
+	int operator()(Image *img) {
+		unsigned char* img_map = img->getMap();
+		int cnt = 0;
+		int h = img->getHeight();
+		int w = img->getWidth();
+		for (int i = 0; i < h * w; ++i)
+		{
+			if (img_map[i] == input_brightness) cnt++;
+		}
+		printf("cnt %d\n", cnt);
+		return cnt;
+	}
+};
+
+class res {
+	int &minb, &maxb, &cnt;
+
+public:
+	res(int &minb, int &maxb, int &cnt) : minb(minb), maxb(maxb), cnt(cnt) {}
+	int operator()(tuple<int, int, int> v) {
+		printf("res %d and %d and %d\n", get<0>(v), get<1>(v), get<2>(v));
 		minb += get<0>(v);
 		maxb += get<1>(v);
+		cnt = get<2>(v);
 		return 0;
 	}
 };
@@ -52,27 +73,33 @@ public:
 int main() {
 	srand(time(NULL));
 
+	unsigned char input_brightness = 100;
+
 	graph g;
 	int max = 0, min = 0;
-	Image *img1 = new Image(3, 3);
+	int cnt_input = 0;
+	Image *img1 = new Image(10, 10);
 	img1->printMap();
 
 	broadcast_node<Image*> s(g);
 	function_node<Image*, int> max_brightness_node(g, unlimited, max_brightness());
 	function_node<Image*, int> min_brightness_node(g, unlimited, min_brightness());
-	join_node<tuple<int, int>, queueing> join(g);
-	function_node<tuple<int, int>, int> result_node(g, serial, res(max, min));
+	function_node<Image*, int> cnt_brightness_node(g, unlimited, cnt_brightness(input_brightness));
+	join_node<tuple<int, int, int>, queueing> join(g);
+	function_node<tuple<int, int, int>, int> result_node(g, serial, res(max, min, cnt_input));
 
 	make_edge(s, max_brightness_node);
 	make_edge(s, min_brightness_node);
+	make_edge(s, cnt_brightness_node);
 	make_edge(max_brightness_node, input_port<0>(join));
 	make_edge(min_brightness_node, input_port<1>(join));
+	make_edge(cnt_brightness_node, input_port<2>(join));
 	make_edge(join, result_node);
 
 	s.try_put(img1);
 	g.wait_for_all();
 
-	printf("Final result is max=%d min=%d\n", max, min);
+	printf("Final result is max=%d min=%d cnt=%d\n", max, min, cnt_input);
 	system("pause");
 	return 0;
 }
