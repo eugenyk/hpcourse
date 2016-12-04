e1850d5b7bf4c78175dd4b6ba19cb7182a77f11c
oxaoo <yol0317@gmail.com>
2016-12-04 15:56:53 +0300

    make array of index target pixs

--- leti/1303/kas/kas_1/kas_1/kas_1.cpp
+++ leti/1303/kas/kas_1/kas_1/kas_1.cpp
@@ -5,44 +5,69 @@
 #include "Image.h"
 
 using namespace tbb::flow;
+using namespace std;
 
 struct max_brightness {
-	int operator()(Image *img) {
+	vector<int> operator()(Image *img) {
 		unsigned char* img_map = img->getMap();
-		unsigned char max = img_map[0];
-		int max_index = 0;
+		unsigned char max = CHAR_MIN;
+		vector<int> arr_max_index;
 		int h = img->getHeight();
 		int w = img->getWidth();
-		for (int i = 1; i < h * w; ++i)
+		for (int i = 0; i < h * w; ++i)
 		{
 			if (img_map[i] > max)
 			{
 				max = img_map[i];
-				max_index = i;
+
+				//cleanup arrary of max index
+				arr_max_index.clear();
+				arr_max_index.push_back(i);
+			}
+			else if (img_map[i] == max)
+			{
+				arr_max_index.push_back(i);
 			}
 		}
-		printf("max=%d, index=%d\n", max, max_index);
-		return max_index;
+		string str = "";
+		for (vector<int>::iterator it = arr_max_index.begin(); it != arr_max_index.end(); ++it)
+		{
+			str += to_string(*it) + " ";
+		}
+		printf("Max value=%d, Array of index of max value: {%s}\n", max, str.c_str());
+		return arr_max_index;
 	}
 };
 
 struct min_brightness {
-	int operator()(Image *img) {
+	vector<int> operator()(Image *img) {
 		unsigned char* img_map = img->getMap();
-		unsigned char min = img_map[0];
-		int min_index = 0;
+		unsigned char min = CHAR_MAX;
+		vector<int> arr_min_index;
 		int h = img->getHeight();
 		int w = img->getWidth();
-		for (int i = 1; i < h * w; ++i)
+		for (int i = 0; i < h * w; ++i)
 		{
 			if (img_map[i] < min)
 			{
 				min = img_map[i];
-				min_index = i;
+
+				//cleanup arrary of min index
+				arr_min_index.clear();
+				arr_min_index.push_back(i);
+			}
+			else if (img_map[i] == min)
+			{
+				arr_min_index.push_back(i);
 			}
 		}
-		printf("min=%d, index=%d\n", min, min_index);
-		return min_index;
+		string str = "";
+		for (vector<int>::iterator it = arr_min_index.begin(); it != arr_min_index.end(); ++it)
+		{
+			str += to_string(*it) + " ";
+		}
+		printf("Min value=%d, Array of index of min value: {%s}\n", min, str.c_str());
+		return arr_min_index;
 	}
 };
 
@@ -52,85 +77,76 @@ class cnt_brightness {
 public:
 	cnt_brightness(unsigned char input_brightness) : input_brightness(input_brightness) {}
 
-	int operator()(Image *img) {
+	vector<int> operator()(Image *img) {
 		unsigned char* img_map = img->getMap();
-		int cnt = 0;
+		vector<int> arr_eq_index;
 		int h = img->getHeight();
 		int w = img->getWidth();
 		for (int i = 0; i < h * w; ++i)
 		{
-			if (img_map[i] == input_brightness) cnt++;
+			if (img_map[i] == input_brightness)
+			{
+				arr_eq_index.push_back(i);
+			}
 		}
-		printf("cnt %d\n", cnt);
-		return cnt;
+		
+		string str = "";
+		for (vector<int>::iterator it = arr_eq_index.begin(); it != arr_eq_index.end(); ++it)
+		{
+			str += to_string(*it) + " ";
+		}
+		printf("User brightness=%d, Array of vaulue's index equals user brightness: {%s}\n", input_brightness, str.c_str());
+		return arr_eq_index;
 	}
 };
 
 class highlight {
-	Image &img;
 public:
-	//highlight(int &minb, int &maxb, int &cnt) : minb(minb), maxb(maxb), cnt(cnt) {}
-	highlight(Image &img) : img(img) {}
-	int operator()(tuple<int, int, int> v) {
-		int max_ind = get<0>(v);
-		int min_ind = get<1>(v);
-		int inp_ind = get<2>(v);
-		printf("highlight max_ind=%d min_ind=%d inp_ind=%d\n", max_ind, min_ind, inp_ind);
-		//minb += get<0>(v);
-		//maxb += get<1>(v);
-		//cnt = get<2>(v);
+	int operator()(tuple<Image*, vector<int>, vector<int>, vector<int>> v) {
+		Image* img = get<0>(v);
+		vector<int> arr_max_ind = get<1>(v);
+		vector<int> arr_min_ind = get<2>(v);
+		vector<int> arr_inp_ind = get<3>(v);
+
+		/*printf("array of max index: (size=%d)\n", arr_max_ind.size());
+		for (vector<int>::iterator it = arr_max_ind.begin(); it != arr_max_ind.end(); ++it)
+		{
+			printf("%d ", *it);
+		}*/
 		return 0;
 	}
 };
 
-//class res {
-//	int &minb, &maxb, &cnt;
-//
-//public:
-//	res(int &minb, int &maxb, int &cnt) : minb(minb), maxb(maxb), cnt(cnt) {}
-//	int operator()(tuple<int, int, int> v) {
-//		printf("res %d and %d and %d\n", get<0>(v), get<1>(v), get<2>(v));
-//		minb += get<0>(v);
-//		maxb += get<1>(v);
-//		cnt = get<2>(v);
-//		return 0;
-//	}
-//};
 
 int main() {
 	srand(time(NULL));
 
+	//todo: get from args
 	unsigned char input_brightness = 100;
 
 	graph g;
-	//int max = 0, min = 0;
-	//int cnt_input = 0;
 	Image *img1 = new Image(10, 10);
 	img1->printMap();
 
 	broadcast_node<Image*> s(g);
-	function_node<Image*, int> max_brightness_node(g, unlimited, max_brightness());
-	function_node<Image*, int> min_brightness_node(g, unlimited, min_brightness());
-	function_node<Image*, int> cnt_brightness_node(g, unlimited, cnt_brightness(input_brightness));
-	join_node<tuple<int, int, int>, queueing> join(g);
-	//todo: delete afte this node.
-	//function_node<tuple<int, int, int>, int> result_node(g, serial, res(max, min, cnt_input));
-
-	function_node<tuple<int, int, int>, int> highlight_node(g, unlimited, highlight(*img1));
+	function_node<Image*, vector<int>> max_brightness_node(g, unlimited, max_brightness());
+	function_node<Image*, vector<int>> min_brightness_node(g, unlimited, min_brightness());
+	function_node<Image*, vector<int>> cnt_brightness_node(g, unlimited, cnt_brightness(input_brightness));
+	join_node<tuple<Image*, vector<int>, vector<int>, vector<int>>, queueing> join(g);
+	function_node<tuple<Image*, vector<int>, vector<int>, vector<int>>, int> highlight_node(g, unlimited, highlight());
 
 	make_edge(s, max_brightness_node);
 	make_edge(s, min_brightness_node);
 	make_edge(s, cnt_brightness_node);
-	make_edge(max_brightness_node, input_port<0>(join));
-	make_edge(min_brightness_node, input_port<1>(join));
-	make_edge(cnt_brightness_node, input_port<2>(join));
-	//make_edge(join, result_node);
+	make_edge(s, input_port<0>(join));
+	make_edge(max_brightness_node, input_port<1>(join));
+	make_edge(min_brightness_node, input_port<2>(join));
+	make_edge(cnt_brightness_node, input_port<3>(join));
 	make_edge(join, highlight_node);
 
 	s.try_put(img1);
 	g.wait_for_all();
 
-	//printf("Final result is max=%d min=%d cnt=%d\n", max, min, cnt_input);
 	system("pause");
 	return 0;
 }
