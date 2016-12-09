6b866e1dfb06ef08c20c20d2f0578566b0d146ad
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2016-12-10 02:46:08 +0300

    Implement argument parsing

--- aptu/604_16/dip/1/main.cpp
+++ aptu/604_16/dip/1/main.cpp
@@ -1,63 +1,45 @@
-#include <tbb/flow_graph.h>
-#include <chrono>
-#include <thread>
+#include <cassert>
+#include <fstream>
 #include <iostream>
 #include <map>
 
+#include <tbb/flow_graph.h>
+
 using namespace tbb::flow;
 
-struct square {
-    int operator()(int v) {
-        printf("squaring %d\n", v);
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        return v * v;
-    }
-};
-
-struct cube {
-    int operator()(int v) {
-        printf("cubing %d\n", v);
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        return v * v * v;
-    }
-};
-
-class sum {
-    int &my_sum;
-public:
-    sum(int &s) : my_sum(s) {}
-
-    int operator()(std::tuple<int, int> v) {
-        printf("adding %d and %d to %d\n", std::get<0>(v), std::get<1>(v), my_sum);
-        my_sum += std::get<0>(v) + std::get<1>(v);
-        return my_sum;
-    }
-};
-
 int main(int argc, char *argv[]) {
-    int result = 0;
-    graph g;
-    broadcast_node<int> input(g);
-    function_node<int, int> squarer(g, unlimited, square());
-    function_node<int, int> cuber(g, unlimited, cube());
-    buffer_node<int> square_buffer(g);
-    buffer_node<int> cube_buffer(g);
-    join_node<std::tuple<int, int>, queueing> join(g);
-    function_node<std::tuple<int, int>, int> summer(g, serial, sum(result));
 
-    make_edge(input, squarer);
-    make_edge(input, cuber);
-    make_edge(squarer, square_buffer);
-    make_edge(squarer, input_port<0>(join));
-    make_edge(cuber, cube_buffer);
-    make_edge(cuber, input_port<1>(join));
-    make_edge(join, summer);
+    // Program can't recover if it receives an odd number of args.
 
-    for (int i = 1; i <= 10; ++i) {
-        input.try_put(i);
+    assert((argc - 1) % 2 == 0);
+
+    ///
+    /// Parse program's arguments.
+    ///
+
+    std::map<std::string, std::string> args;
+
+    for (std::size_t i = 0; i < (argc - 1) / 2; ++i) {
+        args[argv[(i * 2) + 1]] = argv[(i * 2) + 1 + 1];
     }
-    g.wait_for_all();
 
-    printf("Final result is %d\n", result);
+    int brightness = std::stoi(args["-b"]);
+    int limit = std::stoi(args["-l"]);
+    std::ofstream log_file;
+
+    log_file.open(args["-f"]);
+
+    // Program can't recover if the log file can't be opened.
+
+    if (!log_file.is_open()) {
+        std::cout << "Failed to open the log file.\n";
+        return 1;
+    }
+
+    std::cout << "Received arguments:\n";
+    std::cout << " Brightness value: " << brightness << '\n';
+    std::cout << " Concurrency limit: " << limit << '\n';
+    std::cout << " Log file path: " << args["-f"] << '\n';
+
     return 0;
 }
