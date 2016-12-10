bbd325c5aea515a8d814a520b3f57cd2ac9062f6
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2016-12-10 03:43:33 +0300

    Use namespace std

--- aptu/604_16/dip/1/main.cpp
+++ aptu/604_16/dip/1/main.cpp
@@ -6,6 +6,7 @@
 #include <tbb/flow_graph.h>
 
 using namespace tbb::flow;
+using namespace std;
 
 int main(int argc, char *argv[]) {
 
@@ -17,29 +18,29 @@ int main(int argc, char *argv[]) {
     /// Parse program's arguments.
     ///
 
-    std::map<std::string, std::string> args;
+    map<string, string> args;
 
-    for (std::size_t i = 0; i < (argc - 1) / 2; ++i) {
+    for (size_t i = 0; i < (argc - 1) / 2; ++i) {
         args[argv[(i * 2) + 1]] = argv[(i * 2) + 1 + 1];
     }
 
-    int brightness = std::stoi(args["-b"]);
-    int limit = std::stoi(args["-l"]);
-    std::ofstream log_file;
+    int brightness = stoi(args["-b"]);
+    int limit = stoi(args["-l"]);
+    ofstream log_file;
 
     log_file.open(args["-f"]);
 
     // Program can't recover if the log file can't be opened.
 
     if (!log_file.is_open()) {
-        std::cout << "Failed to open the log file.\n";
+        cout << "Failed to open the log file.\n";
         return 1;
     }
 
-    std::cout << "Received arguments:\n";
-    std::cout << " Brightness value: " << brightness << '\n';
-    std::cout << " Concurrency limit: " << limit << '\n';
-    std::cout << " Log file path: " << args["-f"] << '\n';
+    cout << "Received arguments:\n";
+    cout << " Brightness value: " << brightness << '\n';
+    cout << " Concurrency limit: " << limit << '\n';
+    cout << " Log file path: " << args["-f"] << '\n';
 
     return 0;
 }
