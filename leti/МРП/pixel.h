a82f4d4248ed8fe2d3dc8372cea48ee6bb1b15b0
Julia <danilova5.2@yandex.ru>
2017-01-04 14:30:41 +0300

    Update pixel.h

--- leti/МРП/pixel.h
+++ leti/МРП/pixel.h
@@ -1,11 +1,11 @@
-+#ifndef PIXEL_H
-+#define PIXEL_H
-+
-+template <class T>
-+struct pixel {
-+    T value;
-+    int hInd;
-+    int wInd;
-+};
-+
-+#endif // PIXEL_H
+#ifndef PIXEL_H
+#define PIXEL_H
+
+template <class T>
+struct pixel {
+    T value;
+    int hInd;
+    int wInd;
+};
+
+#endif // PIXEL_H
