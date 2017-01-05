75b2b7d27a59a7b92fc7883bd43f7d3e315fc095
e5l <leonid.stashevsky@gmail.com>
2017-01-05 22:08:28 +0300

    fix threads issues

--- aptu/604_16/stashevsky/graph.cc
+++ aptu/604_16/stashevsky/graph.cc
@@ -38,24 +38,24 @@ static void SetBorder(Image& image, Image const& source, Image::pixel value, siz
 }
 
 concurrent_vector<double>
-Measure(size_t height, size_t width, size_t matrix_count, size_t equals_value, size_t concurrency) {
+Measure(size_t height, size_t width, size_t matrix_count, int equals_value, size_t max_images) {
     graph graph_root;
     broadcast_node<Image> source(graph_root);
     concurrent_vector<double> result;
 
     function_node<Image, pair < Image::pixel, Image>>
-    find_max(graph_root, concurrency, [](Image image) {
+    find_max(graph_root, unlimited, [](Image image) {
         Image::pixel maximum = fold(image, 0, [](Image::pixel a, Image::pixel b) { return max(a, b); });
         return make_pair(maximum, image);
     });
 
     function_node<Image, pair < uint8_t, Image>>
-    find_min(graph_root, concurrency, [](Image image) {
+    find_min(graph_root, unlimited, [](Image image) {
         Image::pixel minimum = fold(image, 255, [](Image::pixel a, Image::pixel b) { return min(a, b); });
         return make_pair(minimum, image);
     });
 
-    function_node<pair < uint8_t, Image>, Image > border(graph_root, concurrency, [](pair <uint8_t, Image> stat) {
+    function_node<pair < uint8_t, Image>, Image > border(graph_root, unlimited, [](pair <uint8_t, Image> stat) {
         Image result = stat.second;
         Image::pixel value = stat.first;
         for (size_t i = 0; i < stat.second.height; ++i) {
@@ -69,12 +69,12 @@ Measure(size_t height, size_t width, size_t matrix_count, size_t equals_value, s
 
     broadcast_node<Image> highlighted_caster(graph_root);
 
-    function_node<Image> average(graph_root, concurrency, [&result](Image image) {
+    function_node<Image> average(graph_root, unlimited, [&result](Image image) {
         double sum = fold(image, 0.0, [](double prev, Image::pixel item) { return prev + item; });
         result.push_back(sum / (image.width * image.height));
     });
 
-    function_node<Image, Image> inverse(graph_root, concurrency, [](Image image) {
+    function_node<Image, Image> inverse(graph_root, unlimited, [](Image image) {
         foreach(image, [](Image::pixel& p) {
             p = static_cast<Image::pixel>(0 - p);
         });
@@ -82,12 +82,23 @@ Measure(size_t height, size_t width, size_t matrix_count, size_t equals_value, s
         return image;
     });
 
+    limiter_node<Image> limiter_node(graph_root, max_images);
 
-    make_edge(source, find_max);
-    make_edge(source, find_min);
+    make_edge(source, limiter_node);
+
+    make_edge(limiter_node, find_max);
+    make_edge(limiter_node, find_min);
 
     make_edge(find_max, border);
     make_edge(find_min, border);
+  
+    if (equals_value != -1) {
+        function_node<Image, pair < uint8_t, Image>> find_selected(graph_root, unlimited, [equals_value](Image image) {
+            return make_pair(equals_value, image);
+        });
+        make_edge(source, find_selected);
+        make_edge(find_selected, border);
+    }
 
     make_edge(border, highlighted_caster);
     make_edge(highlighted_caster, average);
--- aptu/604_16/stashevsky/main.cc
+++ aptu/604_16/stashevsky/main.cc
@@ -8,22 +8,22 @@
 using namespace std;
 
 struct Arguments {
-    Image::pixel user_value;
-    size_t max_images;
-    string filename;
+    int user_value = -1;
+    size_t max_images = 1;
+    string filename = "output.txt";
 };
 
 Arguments ParseArguments(int argc, char **argv) {
-    Arguments result;
+    Arguments result = Arguments();
     for (size_t i = 0; i < argc; ++i) {
         string arg(argv[i]);
 
         if (arg == "-b") {
             result.user_value = atoi(argv[i + 1]);
-        }
+        } else
         if (arg == "-l") {
-            result.max_images = atoi(argv[i + 1]);
-        }
+            result.max_images = static_cast<size_t>(atoi(argv[i + 1]));
+        } else
         if (arg == "-f") {
             result.filename = argv[i + 1];
         }
@@ -34,7 +34,7 @@ Arguments ParseArguments(int argc, char **argv) {
 
 int main(int argc, char **argv) {
     auto arguments = ParseArguments(argc, argv);
-    auto result = Measure(128, 128, 10, arguments.max_images);
+    auto result = Measure(1280, 1280, 10000, arguments.user_value, arguments.max_images);
 
     if (arguments.filename.size() == 0) {
         return 0;
