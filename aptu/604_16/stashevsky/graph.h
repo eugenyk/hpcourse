c36fc8b6220fee669af982b9e867d10a2bcd130e
e5l <leonid.stashevsky@gmail.com>
2017-01-05 22:11:12 +0300

    ready

--- aptu/604_16/stashevsky/graph.h
+++ aptu/604_16/stashevsky/graph.h
@@ -3,4 +3,4 @@
 #include <tbb/concurrent_vector.h>
 
 tbb::concurrent_vector<double>
-Measure(size_t height, size_t width, size_t matrix_count, int equals_value, size_t concurrency = 4);
+Measure(size_t height, size_t width, size_t matrix_count, int equals_value, size_t max_images = 4);
