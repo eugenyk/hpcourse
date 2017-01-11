387780f88f39c58ee3ef9b827933b3ddab6bbd9a
oxaoo <yol0317@gmail.com>
2017-01-12 00:36:11 +0300

    chat init commit

---
+++ leti/1303/kas/kas_2/chat/src/main/java/com/github/oxaoo/chat/Main.java
@@ -0,0 +1,12 @@
+package com.github.oxaoo.chat;
+
+/**
+ * @author Alexander Kuleshov
+ * @version 1.0
+ * @since 11.01.2017
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
