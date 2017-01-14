50e82d9d478825a88073310ac8b510dbb241b1b8
Orlovskaya Elizaveta <abovethesky@yandex.ru>
2017-01-14 14:41:55 +0300

    Add images processing. Ready for check.

--- leti/1304/oek/1/lab_01.cpp
+++ leti/1304/oek/1/lab_01.cpp
@@ -7,100 +7,65 @@
 #include <stdio.h>
 #include <string>
 #include <ctime>
+#include <iostream>
+#include <fstream>
 #include <tbb/flow_graph.h>
 
-const size_t IMAGE_WIDTH = 100;
-const size_t IMAGE_HEIGHT = 100;
+#define IMAGE_SIZE 1000
+#define INVALID_IMAGE_ID -1
+#define MAXIMUM_BRIGHT 255
+#define MINIMUM_BRIGHT 0
 
 int num_images = 5;
 int img_number = 0;
 
-// Точка
 class Point
 {
 public:
-    Point()
-    {
-        x = y = 0;
-    }
-    Point(size_t X, size_t Y)
-    {
-        x = X;
-        y = Y;
-    }
-    size_t x;
-    size_t y;
+    Point(): x(0), y(0) {}
+    Point(int _x, int _y): x(_x), y(_y) {}
+    Point(const Point& p): x(p.x), y(p.y) {}
+
+    int x;
+    int y;
 };
 
-// Изображение
 class Image
 {
 public:
-    Image()
+    Image(): id(0) {}
+    Image(int size, int _id): id(_id)
     {
-        width = height = 0;
-        img = nullptr;
-    }
-    Image(size_t w, size_t h)
-    {
-        width = w;
-        height = h;
-        img = new unsigned char* [width];
-        for (size_t i = 0; i < width; i++)
+        for (int i = 0; i < size; i++)
         {
-            img[i] = new unsigned char [height];
-            for (size_t j = 0; j < height; j++)
+            std::vector<unsigned char> v;
+            for (int j = 0; j < size; j++)
             {
-                img[i][j] = rand() % 256;
+                v.push_back(rand() % 256);
             }
+            data.push_back(v);
         }
     }
-    Image(const Image& image)
-    {
-        if (image.img != nullptr)
-        {
-            img = new unsigned char* [image.width];
-            for (size_t i = 0; i < image.width; i++)
-            {
-                img[i] = new unsigned char [image.height];
-                for (size_t j = 0; j < image.height; j++)
-                {
-                    img[i][j] = image.img[i][j];
-                }
-            }
-        }
-    }
-    ~Image()
-    {
-        if (img != nullptr)
-        {
-            for (size_t i = 0; i < width; i++)
-            {
-                delete [] img[i];
-            }
-            delete img;
-            img = nullptr;
-        }
-    }
+    Image(const Image& image): id(image.id), data(image.data) {}
 
-    unsigned char** img;
-    size_t width;
-    size_t height;
+    std::vector< std::vector<unsigned char> > data;
+    int id;
 };
 
-typedef std::pair< Image*, std::vector<Point> > detection_pair;
-typedef std::tuple< detection_pair, detection_pair, detection_pair > detection_tuple;
-typedef std::tuple< unsigned char, unsigned char > output_tuple;
+typedef std::pair<Image, std::vector<Point> > detection_pair;
+typedef std::tuple<detection_pair, detection_pair, detection_pair > detection_tuple;
+typedef std::pair<Image, unsigned char> invert_avg_pair;
+typedef std::tuple<invert_avg_pair, invert_avg_pair> output_tuple;
 
 // Настройки приложения
-unsigned char user_brightness = 0;
+unsigned char searched_bright = 0;
 unsigned char image_processing_limit = 0;
 std::string log_file_name = "";
 
 void print_help()
 {
-    fprintf(stderr, "Usage: flowgraph -b user_brightness -l "
-        "image_processing_limit [-f log_file_name]\n");
+    fprintf(stderr, "Usage: flowgraph -b searched_bright -l "
+        "image_processing_limit [-f log_file_name -n number_of_images]\n");
 }
 
 // Обработка входных параметров
@@ -109,12 +74,12 @@ bool process_input_args(int argc, char *argv[])
     int opt;
     bool b_flag_exist = false;
     bool l_flag_exist = false;
-    while ((opt = getopt(argc, argv, "b:l:f:")) != -1)
+    while ((opt = getopt(argc, argv, "b:l:f:n:")) != -1)
     {
         switch (opt)
         {
         case 'b':
-            user_brightness = atoi(optarg);
+            searched_bright = atoi(optarg);
             b_flag_exist = true;
             break;
         case 'l':
@@ -124,6 +89,9 @@ bool process_input_args(int argc, char *argv[])
         case 'f':
             log_file_name = std::string(optarg);
             break;
+        case 'n':
+            num_images = atoi(optarg);
+            break;
         default:
             print_help();
             exit(EXIT_FAILURE);
@@ -132,21 +100,39 @@ bool process_input_args(int argc, char *argv[])
     return (b_flag_exist && l_flag_exist);
 }
 
-Image *get_next_image()
+Image get_next_image()
 {
     if (img_number < num_images)
     {
         img_number++;
-        return new Image(IMAGE_WIDTH, IMAGE_HEIGHT);
+        return Image(IMAGE_SIZE, img_number);
     }
     else
     {
-        return nullptr;
+        return Image(0, INVALID_IMAGE_ID);
     }
+}
+
+std::vector<Point> searchForPixels(Image& image, unsigned char pixel)
+{
+    std::vector<Point> result;
+    for (int i = 0; i < image.data.size(); i++)
+    {
+        for (int j = 0; j < image.data[i].size(); j++)
+        {
+            if (image.data[i][j] == pixel)
+            {
+                result.push_back(Point(i, j));
+            }
+        }
+    }
+    return result;
 }
 
 int main(int argc, char *argv[])
 {
+    srand(time(NULL));
+
     if (!process_input_args(argc, argv))
     {
         print_help();
@@ -155,125 +141,158 @@ int main(int argc, char *argv[])
 
     tbb::flow::graph g;
 
-    // Source node
-    tbb::flow::source_node< Image* > image_generator(g,
-        [](Image* &next_image) -> bool
+    // Генератор изображений
+    tbb::flow::source_node< Image > image_generator(g,
+        [](Image &next_image) -> bool
         {
             next_image = get_next_image();
-            fprintf(stdout, "=========================================\n");
-            if (next_image != nullptr)
-            {
-                fprintf(stdout, "image_generator: next_image = %p\n", next_image);
-                return true;
-            }
-            else
-            {
-                fprintf(stdout, "image_generator: next_image = %p, return false\n", next_image);
-                return false;
-            }
+            return (next_image.id != INVALID_IMAGE_ID);
         }
     );
 
-    // Limiter node
-    tbb::flow::limiter_node< Image* > limiter(g, image_processing_limit);
+    // Узел, ограничивающий выдачу изображений генератором
+    tbb::flow::limiter_node< Image > limiter(g, image_processing_limit);
 
-    // Maximum detector
-    tbb::flow::function_node< Image*, detection_pair > maximum_detector(
+    // Узел поиска пикселей с максимальной яркостью
+    tbb::flow::function_node< Image, detection_pair > maximum_detector(
         g,
         tbb::flow::unlimited,
-        [](Image* input_image) -> detection_pair
+        [](Image input_image) -> detection_pair
         {
-            std::vector<Point> result;
-
-            fprintf(stdout, "maximum_detector: input_image = %p\n", input_image);
-
+            std::vector<Point> result = searchForPixels(input_image, MAXIMUM_BRIGHT);
             return std::make_pair(input_image, result);
         }
     );
 
-    // Minimum detector
-    tbb::flow::function_node< Image*, detection_pair > minimum_detector(
+    // Узел поиска пикселей с минимальной яркостью
+    tbb::flow::function_node< Image, detection_pair > minimum_detector(
         g,
         tbb::flow::unlimited,
-        [](Image* input_image) -> detection_pair
+        [](Image input_image) -> detection_pair
         {
-            std::vector<Point> result;
-
-            fprintf(stdout, "minimum_detector: input_image = %p\n", input_image);
-
+            std::vector<Point> result = searchForPixels(input_image, MINIMUM_BRIGHT);
             return std::make_pair(input_image, result);
         }
     );
 
-    // User detector
-    tbb::flow::function_node< Image*, detection_pair > user_detector(
+    // Узел поиска пикселей с яркостью, заданной пользователем
+    tbb::flow::function_node< Image, detection_pair > user_detector(
         g,
         tbb::flow::unlimited,
-        [](Image* input_image) -> detection_pair
+        [](Image input_image) -> detection_pair
         {
-            std::vector<Point> result;
-
-            fprintf(stdout, "user_detector: input_image = %p\n", input_image);
-
+            std::vector<Point> result = searchForPixels(input_image, searched_bright);
             return std::make_pair(input_image, result);
         }
     );
 
-    // Detectors join node
-    tbb::flow::join_node< detection_tuple, tbb::flow::queueing > detectors_join(g);
+    // Узел объединения результатов поиска
+    tbb::flow::join_node< detection_tuple, tbb::flow::tag_matching > detectors_join(
+        g,
+        [](const detection_pair &p) -> int { return (int)p.first.id; },
+        [](const detection_pair &p) -> int { return (int)p.first.id; },
+        [](const detection_pair &p) -> int { return (int)p.first.id; }
+    );
 
-    // Draw borders node
-    tbb::flow::function_node< detection_tuple, Image* > draw_borders_node(
+    // Узел обрисовки найденных пикселей рамкой с максимальной яркостью
+    tbb::flow::function_node< detection_tuple, Image > draw_borders_node(
         g,
         tbb::flow::unlimited,
-        [](const detection_tuple& t) -> Image*
+        [](const detection_tuple& t) -> Image
         {
             const detection_pair& max_detect = std::get<0>(t);
             const detection_pair& min_detect = std::get<1>(t);
             const detection_pair& usr_detect = std::get<2>(t);
-            Image* img = max_detect.first;
+            Image img = max_detect.first;
 
-            fprintf(stdout, "draw_borders_node: input_image = %p\n", img);
+            std::vector<Point> allPoints (max_detect.second);
+            allPoints.insert(allPoints.end(), min_detect.second.begin(), min_detect.second.end());
+            allPoints.insert(allPoints.end(), usr_detect.second.begin(), usr_detect.second.end());
+
+            for (int i = 0; i < allPoints.size(); i++)
+            {
+                const Point& p = allPoints[i];
+                // Левый верхний угол
+                if (p.x > 0 && p.y > 0) img.data[p.x - 1][p.y - 1] = MAXIMUM_BRIGHT;
+                // Пиксел сверху
+                if (p.y > 0) img.data[p.x][p.y - 1] = MAXIMUM_BRIGHT;
+                // Правый верхний угол
+                if (p.x < img.data.size() - 1 && p.y > 0) img.data[p.x + 1][p.y - 1] = MAXIMUM_BRIGHT;
+                // Пиксел слева
+                if (p.x > 0) img.data[p.x - 1][p.y] = MAXIMUM_BRIGHT;
+                // Пиксел справа
+                if (p.x < img.data.size() - 1) img.data[p.x + 1][p.y] = MAXIMUM_BRIGHT;
+                // Левый нижний угол
+                if (p.x > 0 && p.y < img.data[p.x - 1].size() - 1) img.data[p.x - 1][p.y + 1] = MAXIMUM_BRIGHT;
+                // Пиксел снизу
+                if (p.y < img.data[p.x].size() - 1) img.data[p.x][p.y + 1] = MAXIMUM_BRIGHT;
+                // Правый нижний угол
+                if (p.x < img.data.size() - 1 && p.y < img.data[p.x + 1].size() - 1) img.data[p.x + 1][p.y + 1] = MAXIMUM_BRIGHT;
+            }
 
             return img;
         }
     );
 
-    // Invert node
-    tbb::flow::function_node< Image*, unsigned char > invert_node(
+    // Узел, инвертирующий изображение
+    tbb::flow::function_node< Image, invert_avg_pair > invert_node(
         g,
         tbb::flow::unlimited,
-        [](Image* input_image) -> unsigned char
+        [](Image input_image) -> invert_avg_pair
         {
-            fprintf(stdout, "invert_node: input_image = %p\n", input_image);
-            return 0;
+            for (int i = 0; i < input_image.data.size(); i++)
+            {
+                for (int j = 0; j < input_image.data[i].size(); j++)
+                {
+                    input_image.data[i][j] = MAXIMUM_BRIGHT - input_image.data[i][j];
+                }
+            }
+            return std::make_pair(input_image, 0);
         }
     );
 
-    // Average node
-    tbb::flow::function_node< Image*, unsigned char > average_node(
+    // Узел поиска среднего значения яркости
+    tbb::flow::function_node< Image, invert_avg_pair > average_node(
         g,
         tbb::flow::unlimited,
-        [](Image* input_image) -> unsigned char
+        [](Image input_image) -> invert_avg_pair
         {
             unsigned char result = 0;
-            fprintf(stdout, "average_node: input_image = %p, average pixel = %u\n",
-                input_image, result);
-            return result;
+            size_t sum = 0;
+            size_t divider = IMAGE_SIZE * IMAGE_SIZE;
+            for (int i = 0; i < input_image.data.size(); i++)
+            {
+                for (int j = 0; j < input_image.data[i].size(); j++)
+                {
+                    sum += (size_t)input_image.data[i][j];
+                }
+            }
+            result = sum / divider;
+            return std::make_pair(input_image, result);
         }
     );
 
-    // Invert & average join node
-    tbb::flow::join_node< output_tuple, tbb::flow::queueing > invert_average_join(g);
+    // Узел объединения результатов инверсии изображения и поиска среднего значения яркости.
+    tbb::flow::join_node< output_tuple, tbb::flow::tag_matching > invert_average_join(
+        g,
+        [](const invert_avg_pair &p) -> int { return (int)p.first.id; },
+        [](const invert_avg_pair &p) -> int { return (int)p.first.id; }
+    );
 
-    // Output node
+    // Узел вывода результата.
     tbb::flow::function_node< output_tuple, tbb::flow::continue_msg > output_node(
         g,
         tbb::flow::unlimited,
         [](const output_tuple& t) -> tbb::flow::continue_msg
         {
-            unsigned char avg_pixel = std::get<1>(t);
-            fprintf(stdout, "output_node: average pixel = %u\n", avg_pixel);
+            invert_avg_pair p = std::get<1>(t);
+            unsigned char avg_pixel = p.second;
+            if (log_file_name.size() > 0)
+            {
+                std::ofstream ofs (log_file_name.c_str(), std::ofstream::out);
+                ofs << std::to_string(avg_pixel);
+                ofs.close();
+            }
             return tbb::flow::continue_msg();
         }
     );
