91fd79ef2b8f716900ed823f21b8158548bda07b
oxaoo <yol0317@gmail.com>
2017-01-16 01:44:52 +0300

    fix form, added host/port

--- leti/1303/kas/kas_2/chat/client/src/main/java/com/github/oxaoo/chat/client/ui/MainForm.java
+++ leti/1303/kas/kas_2/chat/client/src/main/java/com/github/oxaoo/chat/client/ui/MainForm.java
@@ -20,6 +20,10 @@ import java.util.concurrent.ExecutionException;
 public class MainForm {
     private static final Logger LOG = LoggerFactory.getLogger(MainForm.class);
 
+    private static final int DEFAULT_SERVER_PORT = 5432;
+    private static final String DEFAULT_SERVER_HOST = "localhost";
+    public static final String DEFAULT_NAME = "anonymous";
+
     private JPanel mainPanel;
     private JButton sendButton;
     private JTextField messageField;
@@ -28,20 +32,42 @@ public class MainForm {
     private JButton connectButton;
     private JPanel messagePanel;
     private JPanel loginPanel;
+    private JTextField hostField;
+    private JTextField portField;
 
     private Client client;
     private String name;
 
-    public MainForm() {
-        connectButton.addActionListener(e -> {
-            this.name = this.loginField.getText();
-            LOG.info("Name: {}", this.name);
+    private boolean isConnect;
 
-            try {
-                this.client = new Client("localhost", 5432, this);
-            } catch (IOException ex) {
-                LOG.error("Error while create connection. Cause: {}", ex.toString());
-            } catch (InterruptedException | ExecutionException ignored) {
+    public MainForm() {
+        this.isConnect = false;
+
+        connectButton.addActionListener(e -> {
+
+            if (!isConnect) {
+                this.isConnect = true;
+                this.name = this.getName(this.loginField.getText());
+                String host = this.getHost(this.hostField.getText());
+                int port = this.getServerPort(this.portField.getText());
+                LOG.debug("Name: {}", this.name);
+
+                try {
+                    this.client = new Client(host, port, this);
+                } catch (IOException ex) {
+                    LOG.error("Error while create connection. Cause: {}", ex.toString());
+                } catch (InterruptedException | ExecutionException ignored) {
+                }
+
+                connectButton.setText("Disconnect");
+            } else {
+                this.isConnect = false;
+                try {
+                    this.client.closeConnection();
+                } catch (IOException ex) {
+                    LOG.error("Error close server connection. Cause: {}", ex.toString());
+                }
+                connectButton.setText("Connect");
             }
         });
 
@@ -73,13 +99,40 @@ public class MainForm {
         return sdfDate.format(new Date());
     }
 
+    private int getServerPort(String portArg) {
+        int port = DEFAULT_SERVER_PORT;
+        if (portArg != null && !portArg.isEmpty()) {
+            try {
+                port = Integer.parseInt(portArg);
+            } catch (NumberFormatException ignore) {}
+        }
+        if (port < 1024 || port > 65535) port = DEFAULT_SERVER_PORT;
+        return port;
+    }
+
+    private String getHost(String hostArg) {
+        String host = DEFAULT_SERVER_HOST;
+        if (hostArg != null && !hostArg.isEmpty()) {
+            try {
+                host = hostArg;
+            } catch (NumberFormatException ignore) {}
+        }
+        if (host.isEmpty()) host = DEFAULT_SERVER_HOST;
+        return host;
+    }
+
+    private String getName(String name) {
+        if (name == null || name.isEmpty()) return DEFAULT_NAME;
+        return name;
+    }
+
     private static MainForm initForm() {
         MainForm mainForm = new MainForm();
         JFrame frame = new JFrame("Chat");
         frame.setContentPane(mainForm.mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-        frame.setSize(600, 400);
+        frame.setSize(700, 400);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         return mainForm;
