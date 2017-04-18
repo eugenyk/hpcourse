33af601ab1872705ae061a217a530f425802eef8
Egor Suvorov <egor.suvorov@gmail.com>
2017-04-18 22:55:51 +0300

    1: implement producer and consumer

--- aptu/302/suvorov/1/src/main.cpp
+++ aptu/302/suvorov/1/src/main.cpp
@@ -1,39 +1,114 @@
+#include <assert.h>
 #include <pthread.h>
 #include <iostream>
+#include <stdexcept>
+#include <memory>
+#include <vector>
 
-class Value {
+struct NoMoreValuesError : std::runtime_error {
+  NoMoreValuesError() : std::runtime_error("Value class was closed") {}
+};
+
+class PthreadScopedLock {
 public:
-  Value() : _value(0) { }
-
-  void update(int value) {
-    _value = value;
+  PthreadScopedLock(pthread_mutex_t &mutex) : _mutex(mutex) {
+    pthread_mutex_lock(&_mutex);
+  }
+  ~PthreadScopedLock() {
+    pthread_mutex_unlock(&_mutex);
   }
 
-  int get() const {
+private:
+  pthread_mutex_t &_mutex;
+};
+
+// Acts like a barrier: one thread should call update(), another one 
+// should call get(); then both threads will proceed.
+class Value {
+public:
+  Value() : _value(0), _available(false), _is_closed(false) {
+    pthread_mutex_init(&_mutex, NULL);
+    pthread_cond_init(&_cond, NULL);
+  }
+
+  ~Value() {
+    pthread_cond_destroy(&_cond);
+    pthread_mutex_destroy(&_mutex);
+  }
+
+  void update(int value) {
+    PthreadScopedLock l(_mutex);
+    assert(!_available);
+
+    _value = value;
+    _available = true;
+    pthread_cond_signal(&_cond);
+    while (_available) {
+      pthread_cond_wait(&_cond, &_mutex);
+    }
+  }
+
+  void close() {
+    PthreadScopedLock l(_mutex);
+    assert(!_available);
+    _is_closed = true;
+    pthread_cond_signal(&_cond);
+  }
+
+  int get() {
+    PthreadScopedLock l(_mutex);
+    while (!_available && !_is_closed) {
+      pthread_cond_wait(&_cond, &_mutex);
+    }
+    if (!_available) {
+      assert(_is_closed);
+      throw NoMoreValuesError();
+    }
+    _available = false;
+    pthread_cond_signal(&_cond);
     return _value;
   }
  
 private:
   int _value;
+
+  pthread_mutex_t _mutex;
+  pthread_cond_t _cond;
+  bool _available;
+  bool _is_closed;
+};
+
+struct ProducerArg {
+  std::vector<int> data;
+  Value *value;
 };
  
-void* producer_routine(void* arg) {
-  static_cast<void>(arg);
-  // Wait for consumer to start
- 
+void* producer_routine(void* arg_) {
+  ProducerArg *arg = static_cast<ProducerArg*>(arg_);
+  // Wait for consumer to start: TODO
   // Read data, loop through each value and update the value, notify consumer, wait for consumer to process
-
+  for (int x : arg->data) {
+    arg->value->update(x);
+  }
+  arg->value->close();
   return NULL;
 }
  
 void* consumer_routine(void* arg) {
-  static_cast<void>(arg);
-  // notify about start
+  // notify about start: TODO
   // allocate value for result
+  std::unique_ptr<int> result(new int());
   // for every update issued by producer, read the value and add to sum
   // return pointer to result
-
-  return NULL;
+  Value *value = static_cast<Value*>(arg);
+  for (;;) {
+    try {
+      *result += value->get();
+    } catch (NoMoreValuesError&) {
+      break;
+    }
+  }
+  return result.release();
 }
  
 void* consumer_interruptor_routine(void* arg) {
@@ -48,8 +123,26 @@ void* consumer_interruptor_routine(void* arg) {
 int run_threads() {
   // start 3 threads and wait until they're done
   // return sum of update values seen by consumer
-  
-  return 0;
+  Value v;
+  ProducerArg producer_arg;
+  pthread_t producer, consumer;
+
+  for (int i = 0; i < 100000; i++) {
+    producer_arg.data.push_back(i + 1);
+    producer_arg.data.push_back(-i);
+  }
+  producer_arg.value = &v;
+  pthread_create(&producer, NULL, producer_routine, &producer_arg);
+  pthread_create(&consumer, NULL, consumer_routine, &v);
+
+  void *consumer_return;
+  pthread_join(consumer, &consumer_return);
+  std::unique_ptr<int> result(static_cast<int*>(consumer_return));
+  consumer_return = nullptr;
+
+  pthread_join(producer, NULL);
+
+  return *result;
 }
  
 int main() {
