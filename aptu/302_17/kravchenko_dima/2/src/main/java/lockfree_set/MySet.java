ebe6b88f561fedbb3656d87b0d1932f00957f328
equivalence1 <equivalence1@gmail.com>
2017-04-30 22:49:42 +0300

    task 2

---
+++ aptu/302_17/kravchenko_dima/2/src/main/java/lockfree_set/MySet.java
@@ -0,0 +1,130 @@
+package lockfree_set;
+
+public class MySet<T extends Comparable<T>> implements LockFreeSet<T> {
+
+    private final Node<T> head;
+    private final Node<T> tail;
+
+    public MySet() {
+        head = new Node<T>(null);
+        tail = new Node<T>(null);
+        head.next.set(tail, false);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean add(T value) {
+        Node<T> newNode = new Node<T>(value);
+        MyPair<T> pair;
+
+        do {
+            pair = search(value);
+            if ((pair.right != tail) && pair.right.getKey().equals(value)) {
+                return false;
+            }
+            newNode.next.set(pair.right, false);
+            if (pair.left.next.compareAndSet(pair.right, newNode, false, false)) {
+                return true;
+            }
+        } while (true);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean remove(T value) {
+        Node<T> rightNext;
+        MyPair<T> pair;
+
+        do {
+            pair = search(value);
+            if (pair.right == tail || !pair.right.getKey().equals(value))
+                return false;
+            rightNext = pair.right.next.getReference();
+            if (pair.right.next.attemptMark(rightNext, true)) {
+                break;
+            }
+        } while (true);
+
+        boolean ref = pair.left.next.isMarked();
+        if (!pair.left.next.compareAndSet(pair.right, rightNext, ref, ref)) {
+            search(value); // see search -- it also does removes
+        }
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean contains(T value) {
+        MyPair<T> pair = search(value);
+        return (pair.right != tail && pair.right.getKey().equals(value));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean isEmpty() {
+        return head.next.getReference() == tail;
+    }
+
+    /**
+     * finds a pair of adjacent nodes (left, right) so that
+     * left.getKey() <= value <= right.getKey()
+     * @param value
+     * @return
+     */
+    private MyPair<T> search(T value) {
+        Node<T> left = head;
+        Node<T> leftNext = null;
+        Node<T> right;
+
+        do {
+            Node<T> helper = head;
+
+            do {
+                if (!helper.next.isMarked()) {
+                    left = helper;
+                    leftNext = left.next.getReference();
+                }
+                helper = helper.next.getReference();
+                if (helper == tail) {
+                    break;
+                }
+            } while (helper.next.isMarked() || helper.getKey() == null || helper.getKey().compareTo(value) < 0);
+            right = helper;
+            // key should be between left and right
+
+            // check if left and right are adjacent
+            if (leftNext == right) {
+                if (right.next.isMarked()) {
+                    continue;
+                } else {
+                    return new MyPair<T>(left, right);
+                }
+            }
+
+            // leftNext != right => leftNext is marked and we can try remove it
+            boolean ref = left.next.isMarked();
+            if (left.next.compareAndSet(leftNext, right, ref, ref)) {
+                if (right.next.isMarked()) {
+                    continue;
+                } else {
+                    return new MyPair<T>(left, right);
+                }
+            }
+        } while (true);
+    }
+
+    private static class MyPair<T extends Comparable<T>> {
+        private final Node<T> left;
+        private final Node<T> right;
+
+        private MyPair(Node<T> left, Node<T> right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
---
+++ aptu/302_17/kravchenko_dima/2/src/test/java/lockfree_set/MySetTest.java
@@ -0,0 +1,97 @@
+package lockfree_set;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MySetTest {
+
+    @Test
+    public void simpleTest() {
+        for (int t = 0; t < 10; t++) {
+            final MySet<Integer> mySet = new MySet<Integer>();
+
+            List<Integer> nums = new ArrayList<Integer>();
+            for (int i = 0; i < 1000; i++) {
+                nums.add(i);
+            }
+            Collections.shuffle(nums);
+
+            assertTrue(mySet.isEmpty());
+            for (int i : nums) {
+                assertTrue(mySet.add(i));
+            }
+            for (int i : nums) {
+                assertTrue(mySet.contains(i));
+            }
+            assertFalse(mySet.isEmpty());
+            for (int i : nums) {
+                assertTrue(mySet.remove(i));
+            }
+            assertFalse(mySet.remove(123));
+            assertTrue(mySet.isEmpty());
+        }
+    }
+
+    @Test
+    public void multiThreadTest() {
+        for (int test = 0; test < 10; test++) {
+            final MySet<Integer> mySet = new MySet<Integer>();
+
+            List<Integer> nums = new ArrayList<Integer>();
+            for (int i = 0; i < 100; i++) {
+                nums.add(i);
+            }
+            Collections.shuffle(nums);
+
+            List<Thread> threads = new ArrayList<Thread>();
+
+            for (final int i : nums) {
+                Thread t = new Thread(new Runnable() {
+                    public void run() {
+                        assertFalse(mySet.contains(i));
+                        assertTrue(mySet.add(i));
+                        assertTrue(mySet.contains(i));
+                        assertFalse(mySet.add(i));
+                    }
+                });
+                threads.add(t);
+                t.start();
+            }
+
+            joinThreads(threads);
+            Collections.shuffle(nums);
+
+            for (final int i : nums) {
+                Thread t = new Thread(new Runnable() {
+                    public void run() {
+                        assertTrue(mySet.contains(i));
+                        assertFalse(mySet.add(i));
+                        assertTrue(mySet.remove(i));
+                        assertFalse(mySet.contains(i));
+                    }
+                });
+                threads.add(t);
+                t.start();
+            }
+
+            joinThreads(threads);
+
+            assertTrue(mySet.isEmpty());
+        }
+    }
+
+    private void joinThreads(List<Thread> threads) {
+        for (Thread th : threads) {
+            try {
+                th.join();
+            } catch (Exception e) {
+                // blah-blah
+            }
+        }
+    }
+}
