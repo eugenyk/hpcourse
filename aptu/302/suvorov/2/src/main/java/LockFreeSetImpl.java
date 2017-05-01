ec27a06c24233ca03c65bf9d69bbe17c1b89a7a5
Egor Suvorov <egor.suvorov@gmail.com>
2017-05-01 21:29:06 +0300

    2: implement everything lock free

--- aptu/302/suvorov/2/src/main/java/LockFreeSetImpl.java
+++ aptu/302/suvorov/2/src/main/java/LockFreeSetImpl.java
@@ -3,7 +3,10 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> {
     private class Node {
-        // 'next' is marked iff this node is going to be deleted from the list soon.
+        // 'next' is marked iff all of the following are true:
+        // 1. 'next' if the node is not going to change.
+        // 2. The node is going to be deleted.
+        // Invariant: no two consecutive nodes have 'next' marked.
         final AtomicMarkableReference<Node> next;
         final T data;
 
@@ -20,79 +23,82 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
     }
 
     public boolean add(T value) {
+        Node[] nodes = (Node[])new LockFreeSetImpl.Node[2];
         Node newn = new Node(value);
-
         while (true) {
-            Node lastUnmarked = null, lastUnmarkedNext = null;
-            Node prev = root;
-            boolean[] mark = new boolean[1];
-            while (true) {
-                Node cur = prev.next.get(mark);
-                if (!mark[0]) {
-                    lastUnmarked = prev;
-                    lastUnmarkedNext = cur;
-                }
-                if (cur == null) {
-                    if (!lastUnmarked.next.compareAndSet(lastUnmarkedNext, newn, false, false)) {
-                        break;  // Conflict, try again
-                    }
-                    return true;
-                }
-                if (cur.data.equals(value)) {
-                    return false;
-                }
-                prev = cur;
+            search(value, nodes);
+            if (nodes[1] != null) {
+                return false;
             }
+            if (!nodes[0].next.compareAndSet(null, newn, false, false)) {
+                continue;  // Conflict, try again
+            }
+            return true;
         }
     }
 
     public boolean remove(T value) {
+        boolean[] mark = new boolean[1];
+        Node[] nodes = (Node[])new LockFreeSetImpl.Node[2];
+        boolean result = false;
         while (true) {
-            Node lastUnmarked = null, lastUnmarkedNext = null;
-            Node prev = root;
-            boolean[] mark = new boolean[1];
-            while (true) {
-                Node cur = prev.next.get(mark);
-                if (!mark[0]) {
-                    lastUnmarked = prev;
-                    lastUnmarkedNext = cur;
-                }
-
-                if (cur == null) {
-                    return false;
-                }
-                if (cur.data.equals(value)) {
-                    Node next = cur.next.get(mark);
-                    // Do not check mark[0]: even if it's true, then somebody is already trying do
-                    // delete the node - let's help them.
-                    if (!cur.next.attemptMark(next, true)) {
-                        break;  // Conflict, try again.
-                    }
-                    if (!lastUnmarked.next.compareAndSet(lastUnmarkedNext, next, false, false)) {
-                        break; // Conflict, try again
-                    }
-                    // Nobody inserted an element right after us.
-                    assert cur.next.get(mark) == next;
-                    assert mark[0];
-                    return true;
-                }
-                prev = cur;
+            search(value, nodes);
+            if (nodes[1] == null) {
+                return result;
             }
+            Node next = nodes[1].next.get(mark);
+            if (!nodes[1].next.compareAndSet(next, next, false, true)) {
+                continue; // Conflict, try again
+            }
+            result = true;
+            if (!nodes[0].next.compareAndSet(nodes[1], next, false, false)) {
+                continue; // Conflict, try again
+            }
+            return result;
         }
     }
 
     public boolean contains(T value) {
-        Node prev = root;
-        boolean[] mark = new boolean[1];
+        Node[] nodes = (Node[])new LockFreeSetImpl.Node[2];
+        search(value, nodes);
+        return nodes[1] != null;
+    }
+
+    /**
+     * Looks for <code>value</code> in the list. Returns corresponding node in <code>nodes[1]</code>
+     * and its previous node in <code>nodes[0]</code>, if found. If <code>value</code> is not found,
+     * returns last element of the list in <code>nodes[0]</code> and <code>null</code> in <code>nodes[1]</code>.
+     *
+     * Lock-free.
+     */
+    private void search(T value, Node[] nodes) {
+        assert nodes.length == 2;
         while (true) {
-            Node cur = prev.next.get(mark);
-            if (cur == null) {
-                return false;
+            Node prevPrev = null;
+            Node prev = root;
+            boolean[] mark = new boolean[1];
+            while (true) {
+                Node cur = prev.next.get(mark);
+                if (mark[0]) {
+                    if (!prevPrev.next.compareAndSet(prev, cur, false, false)) {
+                        break;  // Conflict, 'cur' was concurrently deleted.
+                    }
+                    prev = prevPrev;
+                    prevPrev = null;
+                }
+                if (cur == null) {
+                    nodes[0] = prev;
+                    nodes[1] = null;
+                    return;
+                }
+                if (cur.data.equals(value)) {
+                    nodes[0] = prev;
+                    nodes[1] = cur;
+                    return;
+                }
+                prevPrev = prev;
+                prev = cur;
             }
-            if (cur.data.equals(value)) {
-                return true;
-            }
-            prev = cur;
         }
     }
 
