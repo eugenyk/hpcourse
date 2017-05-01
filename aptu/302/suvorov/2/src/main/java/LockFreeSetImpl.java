f48b51a23bbdf82bd3dc949ff9146016c241b5b2
Egor Suvorov <egor.suvorov@gmail.com>
2017-05-01 20:26:27 +0300

    2: implement some marking (returns strange results for remove; works otherwise)

--- aptu/302/suvorov/2/src/main/java/LockFreeSetImpl.java
+++ aptu/302/suvorov/2/src/main/java/LockFreeSetImpl.java
@@ -1,12 +1,14 @@
+import java.util.concurrent.atomic.AtomicMarkableReference;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> {
     private class Node {
-        final AtomicReference<Node> next;
+        // 'next' is marked iff this node is going to be deleted from the list soon.
+        final AtomicMarkableReference<Node> next;
         final T data;
 
         private Node(T data) {
-            this.next = new AtomicReference<>();
+            this.next = new AtomicMarkableReference<>(null, false);
             this.data = data;
         }
     }
@@ -20,46 +22,70 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
     public boolean add(T value) {
         Node newn = new Node(value);
 
-        Node prev = root;
         while (true) {
-            Node cur = prev.next.get();
-            if (cur == null) {
-                if (prev.next.compareAndSet(null, newn)) {
-                    return true;
-                } else {
-                    continue;
+            Node lastUnmarked = null, lastUnmarkedNext = null;
+            Node prev = root;
+            boolean[] mark = new boolean[1];
+            while (true) {
+                Node cur = prev.next.get(mark);
+                if (!mark[0]) {
+                    lastUnmarked = prev;
+                    lastUnmarkedNext = cur;
                 }
+                if (cur == null) {
+                    if (!lastUnmarked.next.compareAndSet(lastUnmarkedNext, newn, false, false)) {
+                        break;  // Conflict, try again
+                    }
+                    return true;
+                }
+                if (cur.data.equals(value)) {
+                    return false;
+                }
+                prev = cur;
             }
-            if (cur.data.equals(value)) {
-                return false;
-            }
-            prev = cur;
         }
     }
 
     public boolean remove(T value) {
-        Node prev = root;
         while (true) {
-            Node cur = prev.next.get();
-            if (cur == null) {
-                return false;
-            }
-            if (cur.data.equals(value)) {
-                Node next = cur.next.get();
-                if (prev.next.compareAndSet(cur, next)) {
-                    return true;
-                } else {
-                    continue;
+            Node lastUnmarked = null, lastUnmarkedNext = null;
+            Node prev = root;
+            boolean[] mark = new boolean[1];
+            while (true) {
+                Node cur = prev.next.get(mark);
+                if (!mark[0]) {
+                    lastUnmarked = prev;
+                    lastUnmarkedNext = cur;
                 }
+
+                if (cur == null) {
+                    return false;
+                }
+                if (cur.data.equals(value)) {
+                    Node next = cur.next.get(mark);
+                    // Do not check mark[0]: even if it's true, then somebody is already trying do
+                    // delete the node - let's help them.
+                    if (!cur.next.attemptMark(next, true)) {
+                        break;  // Conflict, try again.
+                    }
+                    if (!lastUnmarked.next.compareAndSet(lastUnmarkedNext, next, false, false)) {
+                        break; // Conflict, try again
+                    }
+                    // Nobody inserted an element right after us.
+                    assert cur.next.get(mark) == next;
+                    assert mark[0];
+                    return true;
+                }
+                prev = cur;
             }
-            prev = cur;
         }
     }
 
     public boolean contains(T value) {
         Node prev = root;
+        boolean[] mark = new boolean[1];
         while (true) {
-            Node cur = prev.next.get();
+            Node cur = prev.next.get(mark);
             if (cur == null) {
                 return false;
             }
@@ -71,6 +97,7 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
     }
 
     public boolean isEmpty() {
-        return root.next.get() == null;
+        boolean[] mark = new boolean[1];
+        return root.next.get(mark) == null;
     }
 }
