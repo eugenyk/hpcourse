8cdff9b239ebce0c3b892fbd634ea04875c3c495
MaxGsomGsom <max_kuzmin@outlook.com>
2017-09-16 13:34:21 +0300

    Added lab2

---
+++ leti/2304/Kuzmin/lockfree_lab/src/com/lockfree/Main.java
@@ -0,0 +1,30 @@
+package com.lockfree;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LockFreeSetImpl<Integer> testSet = new LockFreeSetImpl<>();
+
+        //test
+        new Thread(() -> {
+            for (int i=0; i<100; i++) {testSet.add(i);}
+        }).start();
+
+        new Thread(() -> {
+            for (int i=0; i<100; i+=2) {testSet.remove(i);}
+        }).start();
+
+        new Thread(() -> {
+            for (int i=100; i>0; i--) {testSet.remove(i);}
+        }).start();
+
+        new Thread(() -> {
+            for (int i=100; i>0; i-=2) {testSet.add(i);}
+        }).start();
+
+
+        try { Thread.sleep(1000); } catch (Exception e) { }
+        System.out.print(testSet.getAll().toString());
+    }
+}
