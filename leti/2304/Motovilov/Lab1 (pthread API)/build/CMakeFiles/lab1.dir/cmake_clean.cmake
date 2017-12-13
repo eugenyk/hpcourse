366e92c5ac95ab92d77cf320365827acce02899d
Motovilov Ivan <iv-motovilov@yandex.ru>
2017-12-13 23:30:59 +0300

    add Lab1

---
+++ leti/2304/Motovilov/Lab1 (pthread API)/build/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1.dir/main.cpp.o"
+  "lab1.pdb"
+  "lab1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
