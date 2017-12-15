189054edce623bc5a8abe36a08e181bf42705d1d
MaxGsomGsom <max_kuzmin@outlook.com>
2017-12-15 12:16:56 +0300

    Working lab3

--- leti/2304/Kuzmin/flow_graph_lab/main.cpp
+++ leti/2304/Kuzmin/flow_graph_lab/main.cpp
@@ -5,12 +5,14 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <iostream>
+#include <cstdio>
 
 #define DEBUG
 
 using namespace tbb::flow;
 using namespace std;
 using namespace cimg_library;
+using namespace tbb;
 
 struct Dot {
     int x,y;
@@ -20,33 +22,50 @@ typedef CImg<unsigned char> Cimg;
 typedef tuple<Cimg, string> ImgTuple; //image + name
 typedef tuple<ImgTuple, vector<Dot>, vector<Dot>, vector<Dot>, int, int> ImgDotsTuple; //image + max, min, custom dots + max val, min val
 
-int pink[] = {255, 0, 255}, green[] = {68, 255, 0}, orange[] = { 255, 170, 0 };
+int pink[] = {255, 0, 255}; //max intens
+int green[] = {68, 255, 0}; //min intens
+int orange[] = { 255, 120, 0 }; //custom intens
 
-string intens_file_name;
-int custom_intens, threads;
-ifstream imgs_file;
-ofstream intens_file;
-
+int custom_intens;
 string imgs_file_name = "list.txt";
 string results_dir = "results";
 string imgs_dir = "imgs";
 
 
 //source_node
-struct load_imgs {
+class load_imgs {
+private:
+    vector<string>* imgs_list;
+    int pos;
+public:
+
   bool operator()(ImgTuple& output) {
-    if (!imgs_file.is_open()) imgs_file.open(imgs_dir+"/"+imgs_file_name);
-    string img_name;
-    if (imgs_file >> img_name)
+    //read files list
+    if (imgs_list == NULL) {
+        imgs_list = new vector<string>();
+        pos = 0;
+        ifstream list(imgs_dir+"/"+imgs_file_name);
+        string line;
+        while (list >> line)
+            imgs_list->push_back(line);
+        list.close();
+    }
+
+    //read one image
+    if (pos < imgs_list->size())
     {
+        string img_name = imgs_list->at(pos);
         Cimg data((imgs_dir+"/"+img_name).c_str());
         output = ImgTuple(data, img_name);
+        pos++;
 #ifdef DEBUG
         cout << img_name << " - image loaded" << endl;
 #endif
         return true;
     }
-    imgs_file.close();
+
+    //free memory
+    delete imgs_list;
     return false;
   }
 };
@@ -54,22 +73,22 @@ struct load_imgs {
 //func_node
 struct calc_intens {
   ImgDotsTuple operator()(ImgTuple input) {
-
       Cimg image = get<0>(input);
       vector<Dot> maxs, mins, customs;
       int max_val=0, min_val=255, custom_val=custom_intens;
+int dot_val;
 
-     for (int x=0; x<image.height(); x++) {
-         for (int y=0; y<image.width(); y++){
+     for (int x=0; x<image.width(); x++) {
+         for (int y=0; y<image.height(); y++){
 
-             int dot_val = (image(x,y,0)+image(x,y,1)+image(x,y,2))/3;
+              dot_val = (image(x,y,0)+image(x,y,1)+image(x,y,2))/3;
 
              if (max_val == dot_val) {
                  maxs.push_back({x,y});
              }
              else if (max_val < dot_val) {
                  max_val = dot_val;
-                 maxs.clear();;
+                 maxs.clear();
                  maxs.push_back({x,y});
              }
 
@@ -78,7 +97,7 @@ struct calc_intens {
              }
              else if (min_val > dot_val) {
                  min_val = dot_val;
-                 mins.clear();;
+                 mins.clear();
                  mins.push_back({x,y});
              }
 
@@ -127,7 +146,7 @@ struct select_dots {
           image(dot.x, dot.y, 2) = orange[2];
       }
 
-      file_name="intens_"+file_name;
+      file_name="intensity_"+file_name;
 
 #ifdef DEBUG
       cout << file_name << " - pixels selected on image" << endl;
@@ -137,7 +156,16 @@ struct select_dots {
 };
 
 //func_node
-struct calc_avg_intens {
+class calc_avg_intens {
+private:
+  string file_name;
+  bool file_exists;
+public:
+  calc_avg_intens(string file_name) {
+      this->file_name = file_name;
+      file_exists = true;
+  }
+
   bool operator()(ImgDotsTuple input) {
 
       string img_name = get<1>(get<0>(input));
@@ -148,9 +176,16 @@ struct calc_avg_intens {
 
       int avg = (maxs.size() * max_val + mins.size() * min_val + customs.size() * custom_val) / dots_count;
 
-      if (!intens_file_name.empty()) {
-          if (!intens_file.is_open()) intens_file.open(results_dir+"/"+intens_file_name);
+      if (!file_name.empty()) {
+          if (file_exists) {
+              file_exists = false;
+              remove(("./"+results_dir+"/"+file_name).c_str());
+          }
+
+          ofstream intens_file;
+          intens_file.open("./"+results_dir+"/"+file_name, ios_base::app);
           intens_file << img_name << "=" << avg << endl;
+          intens_file.close();
       }
 
 #ifdef DEBUG
@@ -163,12 +198,11 @@ struct calc_avg_intens {
 //func_node
 struct invert_img {
   ImgTuple operator()(ImgTuple input) {
-
       Cimg image = get<0>(input);
       string file_name = get<1>(input);
 
-      for (int x=0; x<image.height(); x++) {
-          for (int y=0; y<image.width(); y++){
+      for (int x=0; x<image.width(); x++) {
+          for (int y=0; y<image.height(); y++){
 
               image(x,y,0)=255-image(x,y,0);
               image(x,y,1)=255-image(x,y,1);
@@ -176,7 +210,7 @@ struct invert_img {
           }
       }
 
-      file_name="invert_"+file_name;
+      file_name="inverted_"+file_name;
 
 #ifdef DEBUG
         cout << file_name << " - image inverted" << endl;
@@ -189,11 +223,10 @@ struct invert_img {
 //func_node
 struct save_img {
   bool operator()(ImgTuple input) {
-
       Cimg image = get<0>(input);
       string file_name = get<1>(input);
 
-      image.save((results_dir+"/"+file_name).c_str());
+      image.save(("./"+results_dir+"/"+file_name).c_str());
 
 #ifdef DEBUG
         cout << file_name << " - image saved" << endl;
@@ -202,7 +235,12 @@ struct save_img {
   }
 };
 
+
+
 int main(int argc, char *argv[]) {
+
+    string intens_file_name;
+    int threads;
 
     for (int i=1; i<(argc-1); i++) {
         if (strcmp(argv[i], "-b") == 0) custom_intens = strtol(argv[i+1], NULL, 10);
@@ -212,21 +250,22 @@ int main(int argc, char *argv[]) {
 
     if (custom_intens==0 || threads == 0) return 0;
 
+    mkdir(results_dir.c_str(), ACCESSPERMS);
+
     graph g;
     source_node<ImgTuple> load_imgs_node(g, load_imgs(), false);
     broadcast_node<ImgTuple> bcast_imgs(g);
     buffer_node<ImgTuple> calc_intens_buffer(g);
     buffer_node<ImgTuple> invert_buffer(g);
-    buffer_node<ImgDotsTuple> bcast_dots_buffer(g);
     broadcast_node<ImgDotsTuple> bcast_dots(g);
     buffer_node<ImgDotsTuple> select_dots_buffer(g);
     buffer_node<ImgDotsTuple> calc_avg_intens_buffer(g);
     buffer_node<ImgTuple> save_imgs_buffer(g);
     function_node<ImgTuple, ImgDotsTuple> calc_intens_node( g, threads, calc_intens() );
     function_node<ImgDotsTuple, ImgTuple> select_dots_node( g, threads, select_dots() );
-    function_node<ImgDotsTuple, bool> calc_avg_intens_node( g, threads, calc_avg_intens() );
+    function_node<ImgDotsTuple, bool> calc_avg_intens_node( g, serial, calc_avg_intens(intens_file_name) );
     function_node<ImgTuple, ImgTuple> invert_img_node( g, threads, invert_img() );
-    function_node<ImgTuple, bool> save_img_node( g, serial, save_img() );
+    function_node<ImgTuple, bool> save_img_node( g, threads, save_img() );
 
 
     make_edge(load_imgs_node, bcast_imgs);
@@ -238,8 +277,7 @@ int main(int argc, char *argv[]) {
     //to select func
     make_edge(bcast_imgs, calc_intens_buffer);
     make_edge(calc_intens_buffer, calc_intens_node);
-    make_edge(calc_intens_node, bcast_dots_buffer);
-    make_edge(bcast_dots_buffer, bcast_dots);
+    make_edge(calc_intens_node, bcast_dots);
     make_edge(bcast_dots, select_dots_buffer);
     make_edge(select_dots_buffer, select_dots_node);
     make_edge(select_dots_node, save_imgs_buffer);
@@ -249,8 +287,6 @@ int main(int argc, char *argv[]) {
 
     load_imgs_node.activate();
     g.wait_for_all();
-
-    if (intens_file) intens_file.close();
 
     return 0;
 }
