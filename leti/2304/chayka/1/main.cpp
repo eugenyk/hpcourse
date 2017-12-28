1cf2fc0241d548681abd6dae36f2142a2193e856
Konstantin <pro100kot14@gmail.com>
2017-12-28 19:52:29 +0300

    add mutex to 1st lab

--- leti/2304/chayka/1/main.cpp
+++ leti/2304/chayka/1/main.cpp
@@ -1,9 +1,14 @@
 #include <pthread.h>
 #include <iostream>
 
-volatile bool isNeedMore = false;
+//volatile bool isNeedMore = false;
 volatile bool isReady = false;
 volatile bool isFin = false;
+
+pthread_mutex_t producer_mutex;
+pthread_mutex_t consumer_mutex;
+pthread_cond_t producer_signal;
+pthread_cond_t consumer_signal;
 
 class Value {
 public:
@@ -26,12 +31,20 @@ void* producer_routine(void* arg) {
   Value *value = (Value*)arg;
 
   while(std::cin >> n) {
-    while(!isNeedMore){}
-    isNeedMore = false;
     value->update(n);
+    pthread_mutex_lock(&producer_mutex);
     isReady = true;
+    pthread_cond_signal(&producer_signal);
+    pthread_mutex_unlock(&producer_mutex);
+
+    pthread_mutex_lock(&consumer_mutex);
+    while (isReady) {
+        pthread_cond_wait(&consumer_signal, &consumer_mutex);
+    }
+    pthread_mutex_unlock(&consumer_mutex);
   }
   isFin = true;
+  pthread_cond_signal(&producer_signal);
 }
 
 void* consumer_routine(void* arg) {
@@ -39,22 +52,27 @@ void* consumer_routine(void* arg) {
   Value *value = (Value*)arg;
   int *sum = new int;
   *sum = 0;
-  isNeedMore = true;
 
   while(!isFin){
-    while (!isReady) {
-      if(isFin)
-        return sum;
+    pthread_mutex_lock(&producer_mutex);
+     while (!isReady && !isFin) {
+         pthread_cond_wait(&producer_signal, &producer_mutex);
     }
-    isReady = false;
+    pthread_mutex_unlock(&producer_mutex);
+    if (isFin) {
+        break;
+    }
+
     *sum += value->get();
-    isNeedMore = true;
-  }
+
+    pthread_mutex_lock(&consumer_mutex);
+    isReady = false;
+    pthread_cond_signal(&consumer_signal);
+    pthread_mutex_unlock(&consumer_mutex);  }
   return sum;
 }
 
 void* consumer_interruptor_routine(void* arg) {
-  while(!isNeedMore){}
   while (!isFin) {
     pthread_cancel(*(pthread_t*)arg);
   }
@@ -64,6 +82,8 @@ int run_threads() {
   pthread_t producer_thread;
   pthread_t consumer_thread;
   pthread_t consumer_interruptor_thread;
+  pthread_cond_init(&producer_signal, NULL);
+  pthread_cond_init(&consumer_signal, NULL);
   Value value;
   int *sum;
 
