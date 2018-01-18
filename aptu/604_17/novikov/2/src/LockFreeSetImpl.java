0c7b249bf49a415c8f7c6b246091e581c1932c51
PG_LoLo <novgosh@gmail.com>
2018-01-18 20:43:46 +0300

    bugfix + small test

--- aptu/604_17/novikov/2/src/LockFreeSetImpl.java
+++ aptu/604_17/novikov/2/src/LockFreeSetImpl.java
@@ -1,3 +1,7 @@
+import java.util.Collections;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicReference;
 
 class Pair<T1, T2> {
@@ -50,8 +54,11 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
                 if (curr.info.get().marked) {
                     NodeInfo<T> newInfo = new NodeInfo<>(curr.info.get().next, false);
                     if (oldInfo.next == curr && !oldInfo.marked && prev.info.compareAndSet(oldInfo, newInfo)) {
-                        continue restart;
+                        curr = prev.info.get().next;
+                        continue;
                     }
+                    else
+                        continue restart;
                 }
 
                 if (value != null && curr.key.compareTo(value) < 0) {
@@ -75,7 +82,7 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
             Node<T> prev = found.first;
             Node<T> curr = found.second;
 
-            if (curr != null && curr.key == value)
+            if (curr != null && curr.key.compareTo(value) == 0)
                 return false;
             else {
                 NodeInfo<T> oldInfo = prev.info.get();
@@ -124,5 +131,53 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
         Node<T> curr = found.second;
 
         return curr == null;
+    }
+
+    static final int N = 1000;
+    static final int M = 100;
+    static Object[] locks;
+
+    static void run(Set<Integer> set1, LockFreeSet<Integer> set2) {
+        Random random = new Random();
+        while (true) {
+            int value = random.nextInt(N);
+            int op = random.nextInt(3);
+            boolean res1 = false;
+            boolean res2 = false;
+
+            synchronized (locks[value]) {
+                if (op == 0) {
+                    res1 = set1.contains(value);
+                    res2 = set2.contains(value);
+                }
+                else if (op == 1) {
+                    res1 = set1.add(value);
+                    res2 = set2.add(value);
+                }
+                else if (op == 2) {
+                    res1 = set1.remove(value);
+                    res2 = set2.remove(value);
+                }
+            }
+
+            if (res1 != res2)
+                throw new RuntimeException("LOL");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        locks = new Object[N];
+        for (int i = 0; i < N; i++)
+            locks[i] = new Object();
+
+        final Set<Integer> set1 = Collections.newSetFromMap(new ConcurrentHashMap<Integer, Boolean>());
+        final LockFreeSet<Integer> set2 = new LockFreeSetImpl<>();
+
+        Thread last = null;
+        for (int i = 0; i < M; i++) {
+            last = new Thread(() -> run(set1, set2));
+            last.start();
+        }
+        last.join();
     }
 }
