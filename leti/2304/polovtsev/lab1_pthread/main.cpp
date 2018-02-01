8bf694c7d3e7b7c18aed9a2a5d114a0706d60ff6
Sergey1994 <polovtsev-sergey@yandex.ru>
2018-02-01 20:20:56 +0300

    lab1

---
+++ leti/2304/polovtsev/lab1_pthread/main.cpp
@@ -0,0 +1,136 @@
+#include <pthread.h>
+#include <iostream>
+#include <vector>
+#include <ctype.h>
+
+
+class Value
+{
+public:
+  Value() : _value(0) { }
+
+  void update(int value)
+  {
+    _value = value;
+  }
+
+  int get() const
+  {
+    return _value;
+  }
+
+private:
+  int _value;
+};
+
+
+pthread_t producer;
+pthread_t consumer;
+pthread_t interruptor;
+
+pthread_mutex_t mutex;
+pthread_cond_t condition;
+
+volatile bool isUpdateValue = false;
+volatile bool isFinish = false;
+
+std::vector<int> list;
+
+void readData()
+{
+    std::cout << "Input your data: ";
+    while (std::cin.peek() != '\n')
+    {
+        int num;
+        std::cin >> num;
+        if (isdigit(num)) break;
+        list.push_back(num);
+    }
+}
+
+
+void* producer_routine(void* arg)
+{
+    readData();
+
+    std::vector<int>::iterator it = list.begin();
+    while (it != list.end())
+    {
+        pthread_mutex_lock(&mutex);
+        ((Value*)arg)->update(*it);
+        std::cout << "[PROD] value = " << *it << '\n';
+        isUpdateValue = true;
+        pthread_cond_signal(&condition);
+        pthread_mutex_unlock(&mutex);
+        std::cout << "[PROD] mutex unlock \n";
+        ++it;
+    }
+
+    isFinish = true;
+}
+
+void* consumer_routine(void* arg)
+{ 
+    int* sum = new int;
+    *sum = 0;
+
+    pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+
+    while (!isFinish)
+    {
+        pthread_mutex_lock(&mutex);
+
+        while (!isUpdateValue)
+                     pthread_cond_wait(&condition, &mutex);
+
+
+
+        if (isUpdateValue)
+        {
+            *sum += ((Value*)arg)->get();
+            std::cout << "[CONS] sum = " << *sum << '\n';
+            isUpdateValue = false;
+        }
+        pthread_cond_signal(&condition);
+        pthread_mutex_unlock(&mutex);
+        std::cout << "[CONS] mutex unlock\n";
+
+    }
+
+    return (void*)sum;
+}
+
+void* consumer_interruptor_routine(void* arg)
+{
+    while (!isFinish)
+        pthread_cancel(*(pthread_t*) arg);
+}
+
+int run_threads()
+{
+  Value* value = new Value();
+  int* sum;
+
+  pthread_mutex_init(&mutex, NULL);
+  pthread_cond_init(&condition, NULL);
+
+  pthread_create(&consumer, NULL, consumer_routine, value);
+  pthread_create(&producer, NULL, producer_routine, value);
+  pthread_create(&interruptor, NULL, consumer_interruptor_routine, &consumer);
+
+  pthread_join(producer, NULL);
+  pthread_join(consumer, (void**)&sum);
+  pthread_join(interruptor, NULL);
+
+  pthread_mutex_destroy(&mutex);
+  pthread_cond_destroy(&condition);
+
+  delete value;
+  return *sum;
+}
+
+int main()
+{
+  std::cout << "Result = " << run_threads() << std::endl;
+  return 0;
+}
