3c41583953416402c69aff43dbee6c2e720d6f32
Никита Стариков <nikgoust@gmail.com>
2018-02-26 15:06:50 +0300

    IDZ Done
    lockfree list for messages
    Async console writing
    Threadpool for conections

--- leti/2304/Starikov/IDZCs/IDZCs/LockFreeList.cs
+++ leti/2304/Starikov/IDZCs/IDZCs/LockFreeList.cs
@@ -9,21 +9,19 @@ namespace IDZCs
     class LockFreeList{
         public Item Head;   
         public void Add(string text){
-            var item = new Item() {Next = null, Text = text };
-            if (Head == null){
-                if (item == System.Threading.Interlocked.CompareExchange(ref Head, item, null)){
-                    return;
-                }
+            var item = new Item() {Next = null, Text = text };  
+           if (Head == null){
+                if (Head == System.Threading.Interlocked.CompareExchange(ref Head, item, null))return;                
             }
             var prev = Head;
             while (true){
-                if (prev.Next == null){
-                    if (item == System.Threading.Interlocked.CompareExchange(ref prev.Next, item, null)){
-                        return;
-                    }
-                }
-                prev = prev.Next;
-            }
+               if (prev.Next == null){
+                   if (prev.Next == System.Threading.Interlocked.CompareExchange(ref prev.Next, item, null)){
+                       return;
+                   }
+               }
+               prev = prev.Next;
+           }
         }
     }
 
