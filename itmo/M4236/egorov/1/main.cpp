32b7c8b16f70e41d665f195d50c55ad96ce99b2c
Anton <anton1321@mail.ru>
2018-10-23 00:58:07 +0300

    Убран дебажный вывод.
    Можно закомментировать MANUAL и тогда сложатся числа до миллиона.

--- itmo/M4236/egorov/1/main.cpp
+++ itmo/M4236/egorov/1/main.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <random>
 
+#define MANUAL
 
 class Value {
 public:
@@ -29,30 +30,31 @@ pthread_cond_t variable_prepared;
 pthread_t * threads;
 
 int commonConsumersSum;
-//bool hasToWork = true;
 bool consumeProduceFlag = true;
 bool hasToStop = false;
 
 int maxSleepMs;
 
 void* producer_routine(void* arg) {
+    int buffer = 0;
     while (true) {
         pthread_mutex_lock(&mutex);
-        std::cout << "producer: mutex locked\n";
+
         // Wait for consumer to start
         while(consumeProduceFlag) {
             pthread_cond_wait(&ready_for_consume, &mutex);
         }
-        std::cout << "producer asked\n";
+
         // Read data, loop through each value and update the value, notify consumer, wait for consumer to process
-        int buffer;
-        if (!(std::cin >> buffer)) {
-            std::cout << "producer: reading from buffer failed\n";
-            //hasToWork = false;
+        #ifdef MANUAL
+        if (!(std::cin >> buffer))
+        #else
+        if (++buffer > 1e6)
+        #endif
+        {
             pthread_mutex_unlock(&mutex);
             break;
         }
-        std::cout << "buffer was red" << std::endl;
         static_cast<Value*>(arg)->update(buffer);
         consumeProduceFlag = true;
         pthread_cond_signal(&variable_prepared);
@@ -61,15 +63,8 @@ void* producer_routine(void* arg) {
     return nullptr;
 }
 
-void cleanup_handler(void* arg) {
-    std::cout << "cleaning up consumer " + *static_cast<int*>(arg) << std::endl;
-}
 
-volatile int consum_num = 0;
 void* consumer_routine(void* arg) {
-    int tnum = ++consum_num;
-    //pthread_cleanup_push(cleanup_handler, &tnum);
-
     pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, nullptr);
 
     while (true) {
@@ -77,21 +72,19 @@ void* consumer_routine(void* arg) {
         pthread_mutex_lock(&consumer_mutex);
 
         pthread_mutex_lock(&mutex);
-        std::cout << tnum << " consumer: mutex locked\n";
+
         consumeProduceFlag = false;
         pthread_cond_signal(&ready_for_consume);
-        std::cout << tnum << " consumer: notified producer\n";
+
     // for every update issued by producer, read the value and add to sum
         while (!hasToStop && !consumeProduceFlag) {
             pthread_cond_wait(&variable_prepared, &mutex);
         }
         if (hasToStop) {
-            std::cout << tnum << " has to stop\n";
             pthread_mutex_unlock(&mutex);
             pthread_mutex_unlock(&consumer_mutex);
             break;
         }
-        std::cout << "notified about update" << std::endl;
         commonConsumersSum += static_cast<Value*>(arg)->get();
         pthread_mutex_unlock(&mutex);
 
@@ -99,7 +92,6 @@ void* consumer_routine(void* arg) {
         usleep((rand() % maxSleepMs)*1000);
     }
     // return pointer to result (aggregated result for all consumers)
-    //pthread_cleanup_pop(0);
     return &commonConsumersSum;
 }
  
@@ -108,7 +100,7 @@ void* consumer_interruptor_routine(void* arg) {
         int tnum = rand() % *static_cast<int*>(arg);
         int s = pthread_cancel(threads[tnum]);
         if (s != 0) {
-            //std::cout << "interruptor: thread " << tnum << " cancelled with error\n";
+            std::cout << "interruptor: thread " << tnum << " cancelled with error\n";
         }
         pthread_testcancel();
     }  
@@ -137,7 +129,6 @@ int run_threads(int nThreads) {
     pthread_create(&interruptThread, NULL, consumer_interruptor_routine, &nThreads);
     
     pthread_join(prodThread, nullptr);
-    std::cout << "run_threads: joined producer\n";
     hasToStop = true;
     pthread_cond_signal(&variable_prepared);
     
@@ -159,13 +150,20 @@ int run_threads(int nThreads) {
 
     return *(int*)resPointer;
 }
- 
+
+#ifdef MANUAL
 int main(int argc, char* argv[]) {
     int numThreads = atoi(argv[1]);
     
     maxSleepMs = atoi(argv[2]);
-    
-    std::cout << "num threads: " << numThreads << std::endl;
     std::cout << run_threads(numThreads) << std::endl;
     return 0;
 }
+#else
+int main() {
+    int numThreads = 10000;
+    maxSleepMs = 1000;
+    std::cout << run_threads(numThreads) << std::endl;
+    return 0;
+}
+#endif
