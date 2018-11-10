b5f1b84d79135fc0c175964a53da574716598e47
lergor <gor.ler177@gmail.com>
2018-11-10 04:02:01 +0300

    producer and consumers

--- itmo/M4236/goryacheva/1/src/main.cpp
+++ itmo/M4236/goryacheva/1/src/main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 #include <pthread.h>
+#include <vector>
+#include <unistd.h>
+
+#define NOT_STARTED 0
+#define STARTED 1
+#define PRODUCER_READY 2
+#define CONSUMER_READY 3
+#define FINISHED 4
+
+static int MAX_SLEEP_TIME;
+static int CONSUMER_NUMBER;
+
+int status = NOT_STARTED;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t producer_monitor = PTHREAD_COND_INITIALIZER;
+pthread_cond_t consumer_monitor = PTHREAD_COND_INITIALIZER;
 
 class Value {
 public:
@@ -19,31 +36,121 @@ private:
 
 void* producer_routine(void* arg) {
     // Wait for consumer to start
+    if(status == NOT_STARTED) {
+        pthread_mutex_lock(&mutex);
+        while (status != STARTED) {
+            pthread_cond_wait(&producer_monitor, &mutex);
+        }
+        pthread_mutex_unlock(&mutex);
+    }
 
     // Read data, loop through each value and update the value, notify consumer, wait for consumer to process
+    auto value = static_cast<Value *>(arg);
 
+    int num;
+    while (std::cin >> num) {
+        pthread_mutex_lock(&mutex);
+        value->update(num);
+        status = PRODUCER_READY;
+        pthread_cond_broadcast(&consumer_monitor);
+
+        while (status != CONSUMER_READY) {
+            pthread_cond_wait(&producer_monitor, &mutex);
+        }
+        pthread_mutex_unlock(&mutex);
+    }
+
+    status = FINISHED;
+    pthread_cond_broadcast(&consumer_monitor);
+
+    return static_cast<void *>(value);
 }
 
 void* consumer_routine(void* arg) {
     // notify about start
+    if(status == NOT_STARTED) {
+        pthread_mutex_lock(&mutex);
+        status = STARTED;
+        pthread_cond_broadcast(&producer_monitor);
+        pthread_mutex_unlock(&mutex);
+    }
     // for every update issued by producer, read the value and add to sum
     // return pointer to result (aggregated result for all consumers)
+    auto value = static_cast<Value *>(arg);
+    int sum = 0;
+    while (status != FINISHED) {
+        pthread_mutex_lock(&mutex);
+        while (status != FINISHED && status != PRODUCER_READY) {
+            pthread_cond_wait(&consumer_monitor, &mutex);
+        }
+        if(status == FINISHED) {
+            pthread_mutex_unlock(&mutex);
+            break;
+        }
+        sum += value->get();
+        status = CONSUMER_READY;
+        pthread_mutex_unlock(&mutex);
+        pthread_cond_broadcast(&producer_monitor);
+
+        int time = rand() % MAX_SLEEP_TIME;
+        usleep(time * 1000);
+    }
+
+    return new int(sum);
 }
 
 void* consumer_interruptor_routine(void* arg) {
     // wait for consumer to start
 
     // interrupt consumer while producer is running
+
+    return nullptr;
 }
 
 int run_threads() {
     // start N threads and wait until they're done
-    // return aggregated sum of values
+    pthread_t producer;
+    std::vector<pthread_t> consumers(CONSUMER_NUMBER);
+    auto * value = new Value();
+    pthread_create(&producer, nullptr, producer_routine, value);
 
-    return 0;
+    for (auto& consumer: consumers) {
+        pthread_create(&consumer, nullptr, consumer_routine, value);
+    }
+    pthread_join(producer, nullptr);
+
+    // return aggregated sum of values
+    int result = 0;
+    for (auto& consumer: consumers) {
+        int* res;
+        pthread_join(consumer, reinterpret_cast<void**>(&res));
+        result += (*res);
+        delete res;
+    }
+
+    pthread_cond_destroy(&consumer_monitor);
+    pthread_cond_destroy(&producer_monitor);
+    pthread_mutex_destroy(&mutex);
+    return result;
 }
 
-int main() {
+void init(int argc, char* argv[]) {
+    if(argc != 3) {
+        printf("Incorrect arguments number!\n");
+        exit(1);
+    }
+
+    CONSUMER_NUMBER = std::atoi(argv[1]);
+    MAX_SLEEP_TIME = std::atoi(argv[2]);
+
+    if( CONSUMER_NUMBER <= 0 || MAX_SLEEP_TIME <= 0) {
+        printf("Incorrect arguments!\n");
+        exit(1);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    init(argc, argv);
     std::cout << run_threads() << std::endl;
     return 0;
 }
