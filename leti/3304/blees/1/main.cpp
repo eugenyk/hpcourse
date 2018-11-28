bcc13f26cb43f7ebd810fc739959739f86b23d94
EduardBlees <edw252@gmail.com>
2018-11-28 23:57:44 +0300

    cl args

--- leti/3304/blees/1/main.cpp
+++ leti/3304/blees/1/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <pthread.h>
  
-class Value {
+class Value
+{
 public:
     Value() : _value(0) { }
  
-    void update(int value) {
+    void update(int value)
+    {
         _value = value;
     }
  
-    int get() const {
+    int get() const
+    {
         return _value;
     }
  
@@ -17,34 +20,50 @@ private:
     int _value;
 };
  
-void* producer_routine(void* arg) {
+void* producer_routine(void* arg)
+{
   // Wait for consumer to start
  
   // Read data, loop through each value and update the value, notify consumer, wait for consumer to process
- 
 }
  
-void* consumer_routine(void* arg) {
+void* consumer_routine(void* arg)
+{
   // notify about start
   // allocate value for result
   // for every update issued by producer, read the value and add to sum
   // return pointer to result
 }
  
-void* consumer_interruptor_routine(void* arg) {
+void* consumer_interruptor_routine(void* arg)
+{
   // wait for consumer to start
  
   // interrupt consumer while producer is running                                          
 }
  
-int run_threads() {
+int run_threads()
+{
   // start 3 threads and wait until they're done
   // return sum of update values seen by consumer
  
   return 0;
 }
  
-int main() {
+int main(int argc, const char *argv[])
+{
+    if (argc != 3)
+    {
+        std::cout << "Usage:\n\t1st argument - number of consumer-threads\n\t2nd argument - sleep limit in ms\n";
+        return 1;
+    }
+
+    int consumersNum = atoi(argv[1]);
+    int sleepLimit = atoi(argv[2]);
+
+    std::cout << "consumers threads num: " << consumersNum << std::endl;
+    std::cout << "sleepLimit: " << sleepLimit << std::endl;
+
     std::cout << run_threads() << std::endl;
     return 0;
 }
