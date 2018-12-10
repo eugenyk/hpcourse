8e9eedf5e644cf67850e0b62173a698885cdffd0
Bedrang <Bedrang12@yandex.ru>
2018-12-10 20:29:51 +0300

    barrier

--- leti/3304/italyantsev/lab1/main.cpp
+++ leti/3304/italyantsev/lab1/main.cpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <sstream>
 #include <time.h>
- class Value
+class Value
 {
   public:
     Value() : _value(0) {}
@@ -20,17 +20,15 @@
    private:
     int _value;
 };
- // Notify producer about start
 pthread_cond_t tcond = PTHREAD_COND_INITIALIZER;
 volatile bool t_ready = false;
-// Notify consumers about value update
 pthread_mutex_t vmutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t vcond = PTHREAD_COND_INITIALIZER;
 volatile bool v_ready = false;
 volatile bool end = false;
- unsigned n_threads;
+unsigned n_threads;
 unsigned m_sec;
- void *producer_routine(void *arg)
+void *producer_routine(void *arg)
 {
     pthread_mutex_lock(&vmutex);
     while (!t_ready)
@@ -38,10 +36,8 @@ unsigned m_sec;
         pthread_cond_wait(&tcond, &vmutex);
     }
     pthread_mutex_unlock(&vmutex);
-     Value *value_instance = reinterpret_cast<Value *>(arg);
+    Value *value_instance = reinterpret_cast<Value *>(arg);
     std::vector<int> input_values;
-    // Wait for consumer to start
-    // Read data, loop through each value and update the value, notify consumer, wait for consumer to process
     std::string buf;
     std::getline(std::cin, buf);
     std::istringstream ss(buf);
@@ -52,12 +48,10 @@ unsigned m_sec;
     }
      for (int val: input_values)
     {
-        // consumers are ready, update the value
         pthread_mutex_lock(&vmutex);
         value_instance->update(val);
         v_ready = true;
         pthread_cond_broadcast(&vcond);
-        // wait for consumers
         do
         {
             pthread_cond_wait(&tcond, &vmutex);
@@ -71,21 +65,16 @@ unsigned m_sec;
     pthread_cond_broadcast(&vcond);
     v_ready = true;
     pthread_mutex_unlock(&vmutex);
-     pthread_exit(nullptr);
+    pthread_exit(nullptr);
 }
  void *consumer_routine(void *arg)
 {
-    // notify about start
-    // allocate value for result
-    // for every update issued by producer, read the value and add to sum
-    // return pointer to result
     pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, nullptr);
-     static int count = 0;
+    static int count = 0;
     Value *value_instance = reinterpret_cast<Value *>(arg);
     Value *ret_val = new Value();
      while (1)
     {
-        // wait for data and update it
         pthread_mutex_lock(&vmutex);
         count ++;
         if (count >= n_threads)
@@ -110,7 +99,7 @@ unsigned m_sec;
             pthread_exit(reinterpret_cast<void *>(ret_val));
         }
         pthread_mutex_unlock(&vmutex);
-        unsigned time_to_sleep = rand() % m_sec;
+        unsigned time_to_sleep = (m_sec ? (rand() % m_sec) : 0);
         timespec ts;
         ts.tv_sec = (float)time_to_sleep / 1000.0;
         ts.tv_nsec = 0;
@@ -119,35 +108,29 @@ unsigned m_sec;
 }
  void *consumer_interruptor_routine(void *arg)
 {
-    // wait for consumer to start
     pthread_t *consumers = reinterpret_cast<pthread_t *>(arg);
-    // interrupt consumer while producer is running
     while (!end)
     {
         unsigned i = rand() % n_threads;
         pthread_cancel(consumers[i]);
     }
-     pthread_exit(nullptr);
+    pthread_exit(nullptr);
 }
  int run_threads(unsigned threads_cnt, unsigned msec)
 {
-    // start 3 threads and wait until they're done
-    // return sum of update values seen by consumer
     Value *value_instance = new Value();
     Value *ret_val;
     n_threads = threads_cnt;
     m_sec = msec;
-    // creating of 2 + n_threads threads
     pthread_t producer;
     pthread_create(&producer, nullptr, producer_routine, value_instance);
-     pthread_t *consumers = new pthread_t[n_threads];
+    pthread_t *consumers = new pthread_t[n_threads];
     for (size_t i = 0; i < n_threads; i++)
     {
         pthread_create(&consumers[i], nullptr, consumer_routine, value_instance);
     }
-     pthread_t interruptor;
+    pthread_t interruptor;
     pthread_create(&interruptor, nullptr, consumer_interruptor_routine, consumers);
-     // waiting for threads' end
     for (size_t i = 0; i < n_threads; i++)
     {
         pthread_join(consumers[i], reinterpret_cast<void **>(&ret_val));
@@ -155,18 +138,20 @@ unsigned m_sec;
     pthread_join(producer, nullptr);
     pthread_join(interruptor, nullptr);
     delete[] consumers;
-     return ret_val->get();
+    return ret_val->get();
 }
- int main(int argc, const char *argv[])
+
+int main(int argc, const char *argv[])
 {
 using namespace std;
     int threads_cnt, sleep_msec;
-    if (argc < 3)
-    {
-        return EXIT_FAILURE;
+    if (argc <= 2)
+    {   
+        printf("Need more arguments");
+        return 1;
     }
     threads_cnt = atoi(argv[1]);
     sleep_msec = atoi(argv[2]);
-     std::cout << run_threads((unsigned)threads_cnt, (unsigned)sleep_msec) << std::endl;
+    std::cout << run_threads((unsigned)threads_cnt, (unsigned)sleep_msec) << std::endl;
     return 0;
 }
