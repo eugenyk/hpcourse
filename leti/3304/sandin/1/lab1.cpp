7f104510768f81b0db139f37c3ebf7e9a7130621
Artem <art.sandin@yandex.ru>
2018-12-19 01:11:55 +0300

    Sandin Artem, 3304. Add first lab

---
+++ leti/3304/sandin/1/lab1.cpp
@@ -0,0 +1,207 @@
+#include <pthread.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <ctime>
+#include <unistd.h>
+#include <cstdlib>
+
+using namespace std;
+
+int max_sleep_time;
+int count_consumers;
+
+int count_consumers_started = 0;
+int count_consumers_updated = 0;
+
+pthread_mutex_t value_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+pthread_cond_t cond_start_consumer = PTHREAD_COND_INITIALIZER;
+bool consumers_start = false;
+
+pthread_cond_t cond_update_consumer = PTHREAD_COND_INITIALIZER;
+bool consumer_update = false;
+
+pthread_cond_t cond_update_producer = PTHREAD_COND_INITIALIZER;
+int count_producer_update = 0;
+
+bool done = false;
+
+class Value {
+public:
+    Value() : _value(0) { }
+ 
+    void update(int value) {
+        _value = value;
+    }
+ 
+    int get() const {
+        return _value;
+    }
+ 
+private:
+    int _value;
+};
+ 
+void* producer_routine(void* arg) {
+	// Wait for consumer to start
+
+    Value* shared_arg = (Value*) arg;
+    pthread_mutex_lock(&lock);
+    while(!consumers_start) 
+	{
+		pthread_cond_wait(&cond_start_consumer, &lock);
+	}
+	
+    pthread_mutex_unlock(&lock);
+
+	// Read data, loop through each value and update the value, notify consumer, wait for consumer to process
+	string nums;
+    getline(cin, nums);
+    stringstream inStream;
+    inStream << nums;
+	
+    while(!done)
+    {
+        int number;
+        if (inStream >> number)
+        {
+            shared_arg->update(number);
+            count_producer_update++;
+            pthread_cond_broadcast(&cond_update_producer);
+            pthread_mutex_lock(&lock);
+			consumer_update = false;
+            while(!consumer_update) 
+			{
+				pthread_cond_wait(&cond_consumer_update, &lock);
+			}
+            pthread_mutex_unlock(&lock);
+        } 
+		else 
+		{
+            done = true;
+        }
+    }
+	
+    pthread_cond_broadcast(&cond_update_producer);
+}
+ 
+void* consumer_routine(void* arg) {
+	// notify about start
+	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+	Value* shared_arg = (Value*) arg;
+	pthread_mutex_lock(&lock);
+
+	num_of_consumers_started++;
+	if (num_of_consumers_started == count_consumers)
+	{
+	consumers_started = true;
+	pthread_cond_broadcast(&cond_start_consumer);
+	}
+	pthread_mutex_unlock(&lock);
+
+	// for every update issued by producer, read the value and add to sum
+	Value* result = new Value();
+	int count_local_update = 0;
+
+	while(!done)
+	{
+	 pthread_mutex_lock(&lock);
+	 while(!done && (count_local_update == count_producer_update)) 
+	{
+		pthread_cond_wait(&cond_update_producer, &lock);
+	}
+	 pthread_mutex_unlock(&lock);
+	 
+	 if (done)
+	 {
+		 break;
+	 }
+	 
+	 result->update(result->get() + shared_arg->get());
+	 pthread_mutex_lock(&lock);
+	 count_consumers_updated++;
+	 pthread_mutex_unlock(&lock);
+	 count_local_update++;
+	 
+	 if (count_consumers_updated == count_consumers)
+	 {
+		 consumer_update = true;
+		 count_consumers_updated = 0;
+		 pthread_cond_signal(&cond_consumer_update);
+	 }
+	 
+	 usleep(rand() % (max_sleep_time * 1000 + 1));
+	}
+	 
+    // return pointer to result (aggregated result for all consumers)
+    return (void*) result;
+}
+ 
+void* consumer_interruptor_routine(void* arg) {
+	// wait for consumer to start
+    pthread_t* threads = (pthread_t*) arg;
+    pthread_mutex_lock(&lock);
+    while(!consumers_start) 
+	{
+		pthread_cond_wait(&cond_start_consumer, &lock);
+	}
+	
+    pthread_mutex_unlock(&lock);
+	
+	// interrupt consumer while producer is running
+    while(!done)
+    {
+        size_t random_id = rand() % count_consumers;
+        pthread_cancel(threads[random_id]);
+    }
+}
+ 
+int run_threads() {
+  // start N threads and wait until they're done
+  // return aggregated sum of values
+
+	Value* value = new Value();
+    pthread_t producer;
+	pthread_t consumers[count_consumers];
+	pthread_t interruptor;
+	void* res;
+	
+    pthread_create(&producer, 0, producer_routine, value);
+
+	for (int i = 0; i < count_consumers; i++)
+	{
+        pthread_create(&consumers[i], 0, consumer_routine, value);
+	}
+	
+	pthread_create(&interruptor, NULL, consumer_interruptor_routine, &consumers);
+	
+	pthread_join(producer, NULL);
+    pthread_join(interruptor, NULL);
+	pthread_join(consumers[0], &res);
+	
+    for (int i = 1; i < count_consumers; i++)
+	{
+        pthread_join(consumers[i], nullptr);
+    }
+	
+	return *(int *) result;
+}
+ 
+int main(int argc, const char *argv[]) {
+    if (argc != 3)
+    {
+        std::cout << "Incorrect parameters!";
+        return 1;
+    }
+
+    count_consumers = atoi(argv[1]);
+    max_sleep_time = atoi(argv[2]);
+	  
+	srand(time(NULL));
+	
+    cout << run_threads() << endl;
+
+	return 0;
+}
