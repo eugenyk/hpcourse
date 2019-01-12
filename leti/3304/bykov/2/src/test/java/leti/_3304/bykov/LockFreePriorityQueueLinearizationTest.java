dab344abc3def4829f13dd21e602c8e4ea0f0527
Andrei Bykov <dronbykov@gmail.com>
2019-01-12 19:57:06 +0300

    lab 2 Andrei Bykov 3304

---
+++ leti/3304/bykov/2/src/test/java/leti/_3304/bykov/LockFreePriorityQueueLinearizationTest.java
@@ -0,0 +1,40 @@
+package leti._3304.bykov;
+
+import com.devexperts.dxlab.lincheck.LinChecker;
+import com.devexperts.dxlab.lincheck.annotations.Operation;
+import com.devexperts.dxlab.lincheck.annotations.Param;
+import com.devexperts.dxlab.lincheck.paramgen.IntGen;
+import com.devexperts.dxlab.lincheck.strategy.stress.StressCTest;
+import org.junit.Test;
+
+@Param(name = "value", gen = IntGen.class, conf = "1:1000")
+@StressCTest
+public class LockFreePriorityQueueLinearizationTest {
+
+    private LockFreePriorityQueue<Integer> queue = new LockFreePriorityQueue<>();
+
+    @Operation
+    public void offer(@Param(name = "value") int value) {
+        queue.offer(value);
+    }
+
+    @Operation
+    public Integer poll() {
+        return queue.poll();
+    }
+
+    @Operation
+    public Integer peek() {
+        return queue.peek();
+    }
+
+    @Operation
+    public Boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    @Test
+    public void test() {
+        LinChecker.check(LockFreePriorityQueueLinearizationTest.class);
+    }
+}
