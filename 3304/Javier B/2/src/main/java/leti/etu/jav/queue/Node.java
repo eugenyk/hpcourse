ef183183c30be60199324f88859042da26a6cf05
javierferwolf <jbenitezbasantes@gmail.com>
2019-01-16 23:07:32 +0300

    Лаб 2 Хавьер Бенитез 3304

---
+++ 3304/Javier B/2/src/main/java/leti/etu/jav/queue/Node.java
@@ -0,0 +1,20 @@
+package leti.etu.jav.queue;
+
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
+public class Node<E extends Comparable<E>> {
+	public E data = null;
+	public AtomicMarkableReference<Node<E>> next = null;
+	
+	public Node(E data, Node<E> next)
+	{
+		this.data = data;
+		this.next = new AtomicMarkableReference<Node<E>>(next, false);
+	}
+	
+	public Node(E data)
+	{
+		this.data = data;
+		this.next = new AtomicMarkableReference<Node<E>>(null, false);
+	}
+}
---
+++ 3304/Javier B/2/src/test/java/leti/etu/jav/test/LinCheckTest.java
@@ -0,0 +1,56 @@
+package leti.etu.jav.test;
+
+import leti.etu.jav.queue.LockFreePriorityQueue;
+import leti.etu.jav.queue.PriorityQueue;
+
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+
+import com.devexperts.dxlab.lincheck.LinChecker;
+import com.devexperts.dxlab.lincheck.annotations.Operation;
+import com.devexperts.dxlab.lincheck.annotations.Param;
+import com.devexperts.dxlab.lincheck.paramgen.IntGen;
+import com.devexperts.dxlab.lincheck.strategy.stress.StressCTest;
+
+@Param(name = "val", gen = IntGen.class)
+@StressCTest
+public class LinCheckTest {
+	private PriorityQueue<Integer> q = new LockFreePriorityQueue<>();
+
+ 	@Operation
+	public Integer poll() {
+		return q.poll();
+	}
+	
+	@Operation
+	public Integer peek() {
+		return q.peek();
+	}
+
+ 	@Operation
+	public boolean isEmpty() {
+		return q.isEmpty();
+	}
+	/*
+	@Operation
+	public int size() {
+		return q.size();
+	}
+	*/
+	@Operation
+	public boolean add(@Param(name = "val") Integer val) {
+		return q.add(val);
+	}
+
+	@Operation(handleExceptionsAsResult = NoSuchElementException.class)
+	public Integer remove()
+	{
+		return q.remove();
+	}
+
+	@Test
+	public void runTest() {
+		LinChecker.check(LinCheckTest.class);
+	}
+}
