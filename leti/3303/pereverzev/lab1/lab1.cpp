b80cad92e99d1dbcc0c60a4eb4e5288f58c99e50
JonniAl <dapereversev26121995@mail.ru>
2019-01-17 15:18:37 +0300

    fix lab1 by adding barriers

--- leti/3303/pereverzev/lab1/lab1.cpp
+++ leti/3303/pereverzev/lab1/lab1.cpp
@@ -1,27 +1,19 @@
-#include <pthread.h>
 #include <iostream>
-#include <string>
+#include <random>
 #include <sstream>
 #include <ctime>
-#include <vector>
 #include <unistd.h>
 
 using namespace std;
 
-
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 unsigned int sleepLim;
 unsigned int countConsumers;
-pthread_cond_t condConsumerStart = PTHREAD_COND_INITIALIZER;
-bool isConsumerStart = false;
-pthread_cond_t condValueChange = PTHREAD_COND_INITIALIZER;
+
+pthread_barrier_t bThreadsReady;
+pthread_barrier_t bValueReady;
+pthread_barrier_t bConsumersUpdatedValue;
+
 bool isDone = false;
-int countProducerUpdate = 0;
-
-bool isAllConsumerUpdated = false;
-pthread_cond_t condConsumerUpdated = PTHREAD_COND_INITIALIZER;
-
-int countConsumersUpdated = 0;
 
 class Value {
 public:
@@ -40,103 +32,93 @@ private:
 };
 
 void* producer_routine(void* arg) {
+    auto sharedValue = (Value*) arg;
 
-    Value* sharedValue = (Value*) arg;
-  // Wait for consumer to start
-    pthread_mutex_lock(&lock);
-    while(!isConsumerStart) pthread_cond_wait(&condConsumerStart, &lock);
-    pthread_mutex_unlock(&lock);
+    // Wait for consumer to start
+    pthread_barrier_wait(&bThreadsReady);
+
     // Read data
     string input;
     getline(cin, input);
     stringstream inputStream;
     inputStream << input;
+
     // Loop through each value and update the value
+    int curr_val;
     while(!isDone)
     {
-        int curr_val;
         if (inputStream >> curr_val)
         {
             sharedValue->update(curr_val);
-            countProducerUpdate++;
             // Notify consumer
-            isAllConsumerUpdated = false;
-            pthread_cond_broadcast(&condValueChange);
+            pthread_barrier_wait(&bValueReady);
             //Wait for consumer to process
-            pthread_mutex_lock(&lock);
-            while(!isAllConsumerUpdated) pthread_cond_wait(&condConsumerUpdated, &lock);
-            pthread_mutex_unlock(&lock);
+            pthread_barrier_wait(&bConsumersUpdatedValue);
+
         } else {
             isDone = true;
         }
     }
-    pthread_cond_broadcast(&condValueChange);
+    pthread_barrier_wait(&bValueReady);
     return nullptr;
 }
 
 void* consumer_routine(void* arg) {
     pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, nullptr);
-    Value* sharedValue = (Value*) arg;
-  // notify about start
-     pthread_mutex_lock(&lock);
-     countConsumersUpdated++;
-     pthread_mutex_unlock(&lock);
-     if (countConsumersUpdated == countConsumers)
-     {
-         isConsumerStart = true;
-         pthread_cond_broadcast(&condConsumerStart);
-         countConsumersUpdated = 0;
-     }
+    random_device dev;
+    mt19937 gen(dev());
+    uniform_int_distribution<uint> uid(0, (uint)sleepLim);
+    auto sharedValue = (Value*) arg;
 
-  // for every update issued by producer, read the value and add to sum
-     Value* result = new Value();
-     int countValueUpdate = 0;
-     while(!isDone)
-     {
-         pthread_mutex_lock(&lock);
-         while(!isDone && (countValueUpdate == countProducerUpdate)) pthread_cond_wait(&condValueChange, &lock);
-         pthread_mutex_unlock(&lock);
-         if (isDone)
-         {
-             break;
-         }
-         result->update(result->get() + sharedValue->get());
-         pthread_mutex_lock(&lock);
-         countConsumersUpdated++;
-         pthread_mutex_unlock(&lock);
-         countValueUpdate++;
-         if (countConsumersUpdated == countConsumers)
-         {
-             isAllConsumerUpdated = true;
-             countConsumersUpdated = 0;
-             pthread_cond_signal(&condConsumerUpdated);
-         }
-         usleep(rand() % (sleepLim*1000 + 1));
-     }
+    // notify about start
+    pthread_barrier_wait(&bThreadsReady);
+
+    // for every update issued by producer, read the value and add to sum
+    auto result = new Value();
+    while(true)
+    {
+        pthread_barrier_wait(&bValueReady);
+        if (isDone)
+        {
+            break;
+        }
+        result->update(result->get() + sharedValue->get());
+        usleep(uid(gen));
+        pthread_barrier_wait(&bConsumersUpdatedValue);
+    }
     // return pointer to result (aggregated result for all consumers)
-    return (void*) result;
+    return (void*)result;
 
 }
 
 void* consumer_interruptor_routine(void* arg) {
-    vector<pthread_t*>* vectConsumers = (vector<pthread_t*>*) arg;
-  // wait for consumer to start
-    pthread_mutex_lock(&lock);
-    while(!isConsumerStart) pthread_cond_wait(&condConsumerStart, &lock);
-    pthread_mutex_unlock(&lock);
-  // interrupt consumer while producer is running
+    auto vectConsumers = (vector<pthread_t>*) arg;
+    random_device dev;
+    mt19937 gen(dev());
+    uniform_int_distribution<ulong> uid(0, vectConsumers->size() - 1);
+
+    // wait for consumer to start
+    pthread_barrier_wait(&bThreadsReady);
+
+    // interrupt consumer while producer is running
     unsigned long randIDConsumers;
     while(!isDone)
     {
-        randIDConsumers = rand()%vectConsumers->size();
-        pthread_cancel(*vectConsumers->at(randIDConsumers));
+        randIDConsumers = uid(gen);
+        pthread_cancel(vectConsumers->at(randIDConsumers));
     }
     return nullptr;
 }
 
 int run_threads() {
-  // start N threads and wait until they're done
-    Value* value = new Value();
+
+    //Init barriers
+    pthread_barrier_init(&bThreadsReady, nullptr, countConsumers + 2);
+    pthread_barrier_init(&bValueReady, nullptr, countConsumers + 1);
+    pthread_barrier_init(&bConsumersUpdatedValue, nullptr, countConsumers + 1);
+
+    // start N threads and wait until they're done
+    auto value = new Value();
 
     pthread_t producer;
     pthread_create(&producer, nullptr, producer_routine, value);
@@ -152,10 +134,20 @@ int run_threads() {
     pthread_join(interruptor, nullptr);
     void* result;
     pthread_join(consumers[0], &result);
-    for (int i = 1; i < countConsumers; i++)
-        pthread_join(consumers[i], nullptr);
+    auto sum = (Value *)result;
+    int answer = sum->get();
+    delete sum;
+    for (int i = 1; i < countConsumers; i++) {
+        pthread_join(consumers[i], &result);
+        sum = (Value *)result;
+        delete sum;
+    }
+    delete value;
+    pthread_barrier_destroy(&bThreadsReady);
+    pthread_barrier_destroy(&bValueReady);
+    pthread_barrier_destroy(&bConsumersUpdatedValue);
     // return aggregated sum of values
-    return ((Value*)result)->get();
+    return answer;
 
 }
 
@@ -166,8 +158,8 @@ int main(int argc, const char *argv[]) {
         return 1;
     }
 
-    countConsumers = atoi(argv[1]);
-    sleepLim = atoi(argv[2]);
+    countConsumers = (uint)stoi(argv[1]);
+    sleepLim = (uint)stoi(argv[2]);
     cout << run_threads() << endl;
 
     return 0;
