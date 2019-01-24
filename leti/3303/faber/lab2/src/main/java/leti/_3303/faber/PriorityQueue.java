c2467e245ea967926320023183a218e72cb7a8aa
Vadim Faber <faber.vadim@gmail.com>
2019-01-24 14:48:10 +0300

    added lab2

---
+++ leti/3303/faber/lab2/src/main/java/leti/_3303/faber/PriorityQueue.java
@@ -0,0 +1,19 @@
+package leti._3303.faber;
+
+import java.util.Queue;
+
+/**
+ * Lock-Free очередь с приоритетами
+ * @param <T> Тип элементов
+ */
+public interface PriorityQueue<E extends Comparable<E>> extends Queue<E> {
+
+    /**
+     * Проверка очереди на пустоту
+     *
+     * Метод должен быть lock-free (wait-free для уверенных в себе)
+     *
+     * @return true если очередь пуста, иначе - false
+     */
+    boolean isEmpty();
+}
