70c1137e2c034c4ee96f44fdda23ba72da84e88f
s.smolina <s.smolina@tinkoff.ru>
2019-01-29 00:56:45 +0300

    removed comments

--- leti/3303/smolina_sophia/lab2/LockFreePriorityQueue/src/test/ru/sofysmo/priorityqueue/LockFreePriorityQueueLinCheckTest.java
+++ leti/3303/smolina_sophia/lab2/LockFreePriorityQueue/src/test/ru/sofysmo/priorityqueue/LockFreePriorityQueueLinCheckTest.java
@@ -29,33 +29,3 @@ public class LockFreePriorityQueueLinCheckTest {
         LinChecker.check(LockFreePriorityQueueLinCheckTest.class);
     }
 }
-/*import com.devexperts.dxlab.lincheck.LinChecker;
-import com.devexperts.dxlab.lincheck.annotations.Operation;
-import com.devexperts.dxlab.lincheck.annotations.Param;
-import com.devexperts.dxlab.lincheck.strategy.stress.StressCTest;
-import org.junit.Test;
-
-@StressCTest
-public class LockFreePriorityQueueLinCheckTest {
-    private LockFreePriorityQueue<Integer> queue = new LockFreePriorityQueue<>();;
-
-    @Operation
-    public Boolean offer(Integer e) {
-        return queue.offer(e);
-    }
-
-    @Operation
-    public Integer poll() {
-        return queue.poll();
-    }
-
-    @Operation
-    public boolean isEmpty() {
-        return queue.isEmpty();
-    }
-
-    @Test
-    public void test() {
-        LinChecker.check(LockFreePriorityQueueLinCheckTest.class);
-    }
-}*/
