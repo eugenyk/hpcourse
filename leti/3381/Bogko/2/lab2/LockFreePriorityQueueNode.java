d5ba666f7527a1d9dcea36b14e813e695c7af9be
BogkoDima <bogko.dmitry@gmail.com>
2019-02-05 23:02:24 +0300

    lab2 and tests

---
+++ leti/3381/Bogko/2/lab2/LockFreePriorityQueueNode.java
@@ -0,0 +1,22 @@
+package lab2;
+
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
+public class LockFreePriorityQueueNode<E extends   Comparable<E>>{
+
+    public E value;
+    public AtomicMarkableReference<LockFreePriorityQueueNode<E>> next;
+
+    public LockFreePriorityQueueNode(E value)
+    {
+        this.value = value;
+        this.next = new AtomicMarkableReference<>(null, false);
+    }
+
+    public LockFreePriorityQueueNode(E value, LockFreePriorityQueueNode<E> next)
+    {
+        this.value = value;
+        this.next = new AtomicMarkableReference<>(next, false);
+    }
+
+}
