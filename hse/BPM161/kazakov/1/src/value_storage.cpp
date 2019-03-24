85b1411c90b0bbfa06b1c601c1254a68912addff
Dmitry <qudeed@gmail.com>
2019-03-25 02:03:57 +0300

    Deadlock fix

--- hse/BPM161/kazakov/1/src/value_storage.cpp
+++ hse/BPM161/kazakov/1/src/value_storage.cpp
@@ -16,11 +16,11 @@ void value_storage::update(int value) {
 int value_storage::get() {
     pthread_mutex_lock(&value_access);
     while (!has_value_) {
-        pthread_cond_wait(&value_updated, &value_access);
         if (is_closed_ && !has_value_) {
             pthread_mutex_unlock(&value_access);
             throw no_value_exception();
         }
+        pthread_cond_wait(&value_updated, &value_access);
     }
     int current_value = this->value;
     has_value_ = false;
