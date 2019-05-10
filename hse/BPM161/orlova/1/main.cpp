44f7d1bd13463b2d85c709e2b2b9138d0d57d8cf
Александра Орлова <aleksandra.orlova@aleksandraorlova-nb.local>
2019-05-10 15:17:15 +0300

    try fix one more time

--- hse/BPM161/orlova/1/main.cpp
+++ hse/BPM161/orlova/1/main.cpp
@@ -1,5 +1,6 @@
 #include <pthread.h>
 #include <iostream>
+#include <unistd.h>
 #include <vector>
 #include <sstream>
 #include <unistd.h>
@@ -27,11 +28,13 @@ void* producer_routine(void* arg) {
         *((int*)arg) = i;
         has_value = true;
         pthread_cond_signal(&condition_value_change);
-        pthread_cond_wait(&condition_value_change_finish, &condition_value_change_mutext);
+        while (has_value) {
+            pthread_cond_wait(&condition_value_change_finish, &condition_value_change_mutext);
+        }
     }
     is_finish_reading = true;
-    pthread_mutex_unlock(&condition_value_change_mutext);
     pthread_cond_broadcast(&condition_value_change);
+    pthread_mutex_unlock(&condition_value_change_mutext);
     pthread_exit(NULL);
 }
 
@@ -39,83 +42,85 @@ thread_local int part_sum = 0;
 void* consumer_routine(void* arg) {
     pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
     pthread_barrier_wait(&threads_start_barrier);
-    while (true) {
-        pthread_mutex_lock(&condition_value_change_mutext);
-        if (is_finish_reading) {
-            pthread_mutex_unlock(&condition_value_change_mutext);
-            pthread_exit((void*)part_sum);
-        }
+
+    pthread_mutex_lock(&condition_value_change_mutext);
+    while (!is_finish_reading) {
         if (!has_value) {
             pthread_cond_wait(&condition_value_change, &condition_value_change_mutext);
         }
-        if (is_finish_reading) {
-            pthread_mutex_unlock(&condition_value_change_mutext);
-            pthread_exit((void*)part_sum);
-        } else {
+        if (!is_finish_reading) {
             has_value = false;
             part_sum += *((int*)arg);
             *((int*)arg) = 0;
         }
-        pthread_mutex_unlock(&condition_value_change_mutext);
         pthread_cond_signal(&condition_value_change_finish);
         if (sleep_time) {
             sleep(rand() % sleep_time);
         }
     }
+    pthread_mutex_unlock(&condition_value_change_mutext);
+    int* safe_value = new int(part_sum);
+    return safe_value;
 }
 
 void* consumer_interruptor_routine(void* arg) {
     pthread_barrier_wait(&threads_start_barrier);
     pthread_t* threads = (pthread_t*)arg;
-    while (true) {
+
+    pthread_mutex_lock(&condition_value_change_mutext);
+    while (!is_finish_reading) {
         int index = rand() % N;
         pthread_cancel(threads[index]);
-        if (is_finish_reading) {
-            pthread_exit(NULL);
-        }
+        pthread_mutex_unlock(&condition_value_change_mutext);
+        pthread_mutex_lock(&condition_value_change_mutext);
     }
+    pthread_mutex_unlock(&condition_value_change_mutext);
 }
 
 int run_threads(int consumers_number) {
     pthread_t threads[consumers_number];
-    int res[consumers_number];
 
     pthread_mutex_init(&condition_value_change_mutext, NULL);
     pthread_cond_init(&condition_value_change, NULL);
     pthread_cond_init(&condition_value_change_finish, NULL);
-    pthread_barrier_init(&threads_start_barrier, NULL, consumers_number + 2);
+    pthread_barrier_init(&threads_start_barrier, NULL, consumers_number + 3);
 
     pthread_t producer;
     pthread_t interruptor;
-
     pthread_create( &producer, NULL, producer_routine, &value);
     pthread_create( &interruptor, NULL, consumer_interruptor_routine, &threads);
+
     for (int i = 0; i < consumers_number; i++) {
         pthread_create(&(threads[i]), NULL, consumer_routine, &value);
     }
 
-    for (int i = 0; i < consumers_number; i++) {
-        pthread_join(threads[i], (void**)&(res[i]));
-    }
+    pthread_barrier_wait(&threads_start_barrier);
 
     pthread_join(producer, NULL);
     pthread_join(interruptor, NULL);
+    int sum = 0;
+    for (int i = 0; i < consumers_number; i++) {
+        void* number;
+        pthread_join(threads[i], &number);
+        sum += *((int*)number);
+        delete number;
+    }
+
     pthread_mutex_destroy(&condition_value_change_mutext);
     pthread_cond_destroy(&condition_value_change);
     pthread_cond_destroy(&condition_value_change_finish);
     pthread_barrier_destroy(&threads_start_barrier);
 
-    int sum = 0;
-    for (int i = 0 ; i < consumers_number; i++) {
-        sum += res[i];
-    }
-
     return sum;
 }
 
-int main(int argc, char **argv) {
+void getArgs(char **argv) {
     N = stoi(argv[1]);
     sleep_time = stoi(argv[2]);
+}
+
+int main(int argc, char **argv) {
+    getArgs(argv);
     cout << run_threads(N) << endl;
     return 0;
 }
