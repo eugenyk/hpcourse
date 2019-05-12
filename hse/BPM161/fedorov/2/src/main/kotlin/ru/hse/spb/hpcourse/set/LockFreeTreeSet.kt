e202f2d31d46c7d8ee2a327621e6a0add8f88ffe
Alexandr Fedorov <fedorov2602@mail.ru>
2019-05-12 23:29:20 +0300

    LockFreeSet initials

---
+++ hse/BPM161/fedorov/2/src/main/kotlin/ru/hse/spb/hpcourse/set/LockFreeTreeSet.kt
@@ -0,0 +1,23 @@
+package ru.hse.spb.hpcourse.set
+
+class LockFreeTreeSet<T : Comparable<T>> : LockFreeSet<T> {
+    override fun isEmpty(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun contains(value: T): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun add(value: T): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun remove(value: T): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun iterator(): MutableIterator<T> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
