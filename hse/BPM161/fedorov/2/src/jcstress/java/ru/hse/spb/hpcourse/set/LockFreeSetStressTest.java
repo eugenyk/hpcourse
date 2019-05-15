09282dd0adedf17dc63cc5e9c201e950df2ce8fb
Alexandr Fedorov <fedorov2602@mail.ru>
2019-05-15 23:54:37 +0300

    More jcstress tests

--- hse/BPM161/fedorov/2/src/jcstress/java/ru/hse/spb/hpcourse/set/LockFreeSetStressTest.java
+++ hse/BPM161/fedorov/2/src/jcstress/java/ru/hse/spb/hpcourse/set/LockFreeSetStressTest.java
@@ -1,7 +1,9 @@
 package ru.hse.spb.hpcourse.set;
 
 import org.openjdk.jcstress.annotations.*;
+import org.openjdk.jcstress.infra.results.FFF_Result;
 import org.openjdk.jcstress.infra.results.ZZZZ_Result;
+import org.openjdk.jcstress.infra.results.ZZZ_Result;
 import org.openjdk.jcstress.infra.results.ZZ_Result;
 
 import static org.openjdk.jcstress.annotations.Expect.ACCEPTABLE;
@@ -12,7 +14,7 @@ public class LockFreeSetStressTest {
     @JCStressTest
     @Outcome(id = "true, true, true, true", expect = ACCEPTABLE, desc = "All values successfully added.")
     @State
-    public static class ConcurrentAddTest {
+    public static class ConcurrentRaceTest {
         private LockFreeSet<Integer> set = new LockFreeSet<>();
 
         @Actor
@@ -35,7 +37,6 @@ public class LockFreeSetStressTest {
             set.add(4);
         }
 
-
         @Arbiter
         public void arbiter(ZZZZ_Result r) {
             r.r1 = set.contains(1);
@@ -44,6 +45,7 @@ public class LockFreeSetStressTest {
             r.r4 = set.contains(4);
         }
     }
+
 
     @JCStressTest
     @Outcome(id = "true, true", expect = ACCEPTABLE, desc = "All values successfully added before queries.")
@@ -64,6 +66,72 @@ public class LockFreeSetStressTest {
         public void actor2(ZZ_Result r) {
             r.r2 = set.contains(2);
             r.r1 = set.contains(1);
+        }
+    }
+
+    @JCStressTest
+    @Outcome(id = "true, false, false, true", expect = ACCEPTABLE, desc = "First add succeeded, remove didn't.")
+    @Outcome(id = "true, false, true, false", expect = ACCEPTABLE, desc = "First add succeeded, so did remove.")
+    @Outcome(id = "false, true, false, true", expect = ACCEPTABLE, desc = "Second add succeeded, remove didn't.")
+    @Outcome(id = "false, true, true, false", expect = ACCEPTABLE, desc = "Second add succeeded, so did remove.")
+    @Outcome(id = "true, true, true, true", expect = ACCEPTABLE, desc = "All queries succeeded.")
+    @State
+    public static class SameValueRaceTest {
+        private LockFreeSet<Integer> set = new LockFreeSet<>();
+
+        @Actor
+        public void actor1(ZZZZ_Result r) {
+            r.r1 = set.add(1);
+        }
+
+        @Actor
+        public void actor2(ZZZZ_Result r) {
+            r.r2 = set.add(1);
+        }
+
+        @Actor
+        public void actor3(ZZZZ_Result r) {
+            r.r3 = set.remove(1);
+        }
+
+
+        @Arbiter
+        public void arbiter(ZZZZ_Result r) {
+            r.r4 = set.contains(1);
+        }
+    }
+
+    @JCStressTest
+    @Outcome(id = "true, true, true, true", expect = ACCEPTABLE, desc = "Successfully removed everything.")
+    @State
+    public static class RemoveEmptyTest {
+        private LockFreeSet<Integer> set = new LockFreeSet<>();
+
+        RemoveEmptyTest() {
+            set.add(1);
+            set.add(2);
+            set.add(3);
+        }
+
+        @Actor
+        public void actor1(ZZZZ_Result r) {
+            r.r1 = set.remove(1);
+        }
+
+        @Actor
+        public void actor2(ZZZZ_Result r) {
+            r.r2 = set.remove(2);
+        }
+
+        @Actor
+        public void actor3(ZZZZ_Result r) {
+            r.r3 = set.remove(3);
+        }
+
+
+        @Arbiter
+        public void arbiter(ZZZZ_Result r) {
+            r.r4 = set.isEmpty();
         }
     }
 }
