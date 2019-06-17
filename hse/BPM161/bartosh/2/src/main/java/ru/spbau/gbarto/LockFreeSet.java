2a24e4154f1f24f6131649c47aba2044469ee079
Grigory Bartosh <grishabartosh@yandex.ru>
2019-06-17 17:34:46 +0300

    fix lab 2

--- hse/BPM161/bartosh/2/src/main/java/ru/spbau/gbarto/LockFreeSet.java
+++ hse/BPM161/bartosh/2/src/main/java/ru/spbau/gbarto/LockFreeSet.java
@@ -3,24 +3,33 @@ package ru.spbau.gbarto;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicMarkableReference;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 public class LockFreeSet<T extends Comparable<T>> implements LockFreeSetInterface<T> {
     private enum NodeType {HEAD, TAIL, USUAL}
+    private final static int MARKED = 1;
+    private final static int UNMARKED = 0;
+
+    private final AtomicInteger version;
 
     private class Node {
         private T value;
         private NodeType type;
-        private final AtomicMarkableReference<Node> next = new AtomicMarkableReference<>(null, false);
+        private AtomicStampedReference<Node> next;
 
         private Node(T value) {
             this.value = value;
             this.type = NodeType.USUAL;
+            int v = version.getAndIncrement();
+            this.next = new AtomicStampedReference<>(null, 2 * v + UNMARKED);
         }
 
         private Node(T value, NodeType type) {
             this.value = value;
             this.type = type;
+            int v = version.getAndIncrement();
+            this.next = new AtomicStampedReference<>(null, 2 * v + UNMARKED);
         }
 
         private T getValue() {
@@ -32,17 +41,25 @@ public class LockFreeSet<T extends Comparable<T>> implements LockFreeSetInterfac
         }
 
         private void setNext(Node next) {
-            this.next.set(next, false);
+            int v = version.getAndIncrement();
+            this.next.set(next, 2 * v + UNMARKED);
         }
 
         private boolean isMarked() {
-            return next.isMarked();
+            return next.getStamp() % 2 == 1;
+        }
+
+        private int getVersion() {
+            return next.getStamp() / 2;
         }
 
         private boolean compareAndSet(Node expectedReference,
-                              Node newReference,
-                              boolean newMark) {
-            return next.compareAndSet(expectedReference, newReference, false, newMark);
+                                      Node newReference,
+                                      boolean newMark) {
+            int expectedStamp = (this.next.getStamp() / 2) * 2 + UNMARKED;
+            int v = version.getAndIncrement();
+            int newStamp = v * 2 + (newMark ? MARKED : UNMARKED);
+            return next.compareAndSet(expectedReference, newReference, expectedStamp, newStamp);
         }
         
         private int compare(T value) {
@@ -68,24 +85,53 @@ public class LockFreeSet<T extends Comparable<T>> implements LockFreeSetInterfac
             this.second = second;
         }
     }
-    
+
+    private class PairNodeVersion {
+        private Node node;
+        private int version;
+
+        private PairNodeVersion(Node node, int version) {
+            this.node = node;
+            this.version = version;
+        }
+    }
+
     private Node head;
     private Node tail;
 
     public LockFreeSet() {
+        version = new AtomicInteger(0);
         head = new Node(null, NodeType.HEAD);
         tail = new Node(null, NodeType.TAIL);
         head.setNext(tail);
     }
 
-    private List<Node> toList() {
-        List<Node> list = new ArrayList<>();
+    private List<PairNodeVersion> toList() {
+        List<PairNodeVersion> list = new ArrayList<>();
         for (Node n = head.getNext(); n != tail; n = n.getNext()) {
             if (!n.isMarked()) {
-                list.add(n);
+                list.add(new PairNodeVersion(n, n.getVersion()));
             }
         }
         return list;
+    }
+
+    private boolean compareListsNodeVersion(List<PairNodeVersion> list1, List<PairNodeVersion> list2) {
+        if (list1.size() != list2.size()) {
+            return false;
+        }
+
+        Iterator<PairNodeVersion> it1 = list1.iterator();
+        Iterator<PairNodeVersion> it2 = list2.iterator();
+        while (it1.hasNext()) {
+            PairNodeVersion p1 = it1.next();
+            PairNodeVersion p2 = it2.next();
+            if (p1.node != p2.node || p1.version != p2.version) {
+                return false;
+            }
+        }
+
+        return true;
     }
     
     private PairNode find(T value) {
@@ -163,23 +209,29 @@ public class LockFreeSet<T extends Comparable<T>> implements LockFreeSetInterfac
 
     @Override
     public boolean isEmpty() {
-        for (Node n = head.getNext(); n != tail; n = n.getNext()) {
-            if (!n.isMarked()) {
-                return false;
+        while (true) {
+            Node n = head;
+            Node nc = n.getNext();
+
+            if (nc.isMarked()) {
+                Node ncc = nc.getNext();
+                n.compareAndSet(nc, ncc, false);
+            } else {
+                return (nc == tail);
             }
+
         }
-        return true;
     }
 
     @Override
     public Iterator<T> iterator() {
-        List<Node> list1;
-        List<Node> list2;
+        List<PairNodeVersion> list1;
+        List<PairNodeVersion> list2;
         do {
             list1 = toList();
             list2 = toList();
-        } while (!list1.equals(list2));
+        } while (!compareListsNodeVersion(list1, list2));
 
-        return list1.stream().map(Node::getValue).iterator();
+        return list1.stream().map(p -> p.node.getValue()).iterator();
     }
 }
